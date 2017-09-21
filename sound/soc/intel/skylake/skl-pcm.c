566b0a8a48187d84f080266d0364f38e96e2bd18
Vinod Kumar <vinod.kumarx.vinod.kumar@intel.com>
2017-09-21 09:52:34 +0800

    ASoC: Intel: Skylake: Add support of S24_3LE PCM formats for FE
    Add support of missing S24_3LE PCM formats for System Pin 6,
    Deepbuffer and Reference Pin of FE CPU DAI.
    
    Signed-off-by: Vinod Kumar <vinod.kumarx.vinod.kumar@intel.com>
    
    Conflicts:
    	sound/soc/intel/skylake/skl-pcm.c

--- sound/soc/intel/skylake/skl-pcm.c
+++ sound/soc/intel/skylake/skl-pcm.c
@@ -716,8 +716,9 @@ static struct snd_soc_dai_driver skl_fe_dai[] = {
 		.stream_name = "Reference Capture",
 		.channels_min = HDA_MONO,
 		.channels_max = HDA_QUAD,
-		.rates = SNDRV_PCM_RATE_48000 | SNDRV_PCM_RATE_16000,
-		.formats = SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE,
+		.rates = SNDRV_PCM_RATE_8000_192000 | SNDRV_PCM_RATE_KNOT,
+		.formats = SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE |
+			SNDRV_PCM_FMTBIT_S32_LE | SNDRV_PCM_FMTBIT_S24_3LE,
 		.sig_bits = 32,
 	},
 },
@@ -729,7 +730,8 @@ static struct snd_soc_dai_driver skl_fe_dai[] = {
 		.channels_min = HDA_STEREO,
 		.channels_max = HDA_STEREO,
 		.rates = SNDRV_PCM_RATE_48000,
-		.formats = SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE,
+		.formats = SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE |
+			SNDRV_PCM_FMTBIT_S32_LE | SNDRV_PCM_FMTBIT_S24_3LE,
 		.sig_bits = 32,
 	},
 },
