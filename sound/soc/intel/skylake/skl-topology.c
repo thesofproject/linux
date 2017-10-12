4063118a98359e2839a9ca079c46dbfad9e43dde
Vinod Kumar <vinod.kumarx.vinod.kumar@intel.com>
2017-10-12 18:28:05 +0800

    ASoC: Intel: Skylake: Checking the dapm widgtet before the recursive call
    Kernel was crashing due to NULL pointer dereference of dapm widget pointer
    while playing the audio file with multiple pipeline configuration topology
    
    This patch is to make sure to check the dapm widget pointer before the
    every recursive call.
    
    Signed-off-by: Vinod Kumar <vinod.kumarx.vinod.kumar@intel.com>
    
    Conflicts:
    	sound/soc/intel/skylake/skl-topology.c

--- sound/soc/intel/skylake/skl-topology.c
+++ sound/soc/intel/skylake/skl-topology.c
@@ -1115,7 +1115,11 @@ static int skl_tplg_bind_sinks(struct snd_soc_dapm_widget *w,
 		}
 	}
 
-	if (!sink && next_sink)
+	/* NOTE: While selecting the multiple pipeline confguration next_sink
+	 * become NULL. So sink and next_sink checking is required for muliple
+	 * pipeline configuration.
+	 */
+	if ((!sink) && (next_sink))
 		return skl_tplg_bind_sinks(next_sink, skl, src_w, src_mconfig);
 
 	return 0;
