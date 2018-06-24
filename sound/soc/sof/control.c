132dfa5fa32e65de29ad11b143561e99321e380a
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2018-06-23 22:31:11 -0700

    Add support for triggering siggen pipeline from switch IO handler
    This patch adds the changes to the switch control IO handler to
    handle the additional steps required to trigger the siggen
    pipeline based on user input.
    
    When the user switches the siggen control ON, the IO handler sends
    the enables BE DAI, and sends ipc messages to set up the pcm params
    and start the pipeline.
    
    When the user switches it off, it de-activates the virtual FE
    link, frees the pcm and resets the pipeline.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- sound/soc/sof/control.c
+++ sound/soc/sof/control.c
@@ -35,6 +35,127 @@ static int get_widget_type(struct snd_sof_dev *sdev,
 	return -EINVAL;
 }
 
+/* helper function to send pcm params ipc */
+static int siggen_pcm_params(struct snd_sof_control *scontrol,
+			     struct snd_sof_dev *sdev)
+{
+	struct sof_ipc_pcm_params_reply ipc_params_reply;
+	struct sof_ipc_pcm_params pcm;
+	int ret = 0;
+
+	memset(&pcm, 0, sizeof(pcm));
+
+	/* set IPC PCM parameters */
+	pcm.hdr.size = sizeof(pcm);
+	pcm.hdr.cmd = SOF_IPC_GLB_STREAM_MSG | SOF_IPC_STREAM_PCM_PARAMS;
+	pcm.comp_id = scontrol->comp_id;
+	pcm.params.channels = scontrol->num_channels;
+	pcm.params.direction = SOF_IPC_STREAM_PLAYBACK;
+
+	/* send IPC to the DSP */
+	ret = sof_ipc_tx_message(sdev->ipc, pcm.hdr.cmd, &pcm, sizeof(pcm),
+				 &ipc_params_reply, sizeof(ipc_params_reply));
+	if (ret < 0)
+		dev_err(sdev->dev, "error: setting pcm params for siggen\n");
+
+	return ret;
+}
+
+/* helper function to send stream trigger ipc for siggen pipeline */
+static int siggen_trigger(struct snd_sof_control *scontrol,
+			  struct snd_sof_dev *sdev, int cmd)
+{
+	struct sof_ipc_stream stream;
+	struct sof_ipc_reply reply;
+	int ret = 0;
+
+	/* set IPC stream params */
+	stream.hdr.size = sizeof(stream);
+	stream.hdr.cmd = SOF_IPC_GLB_STREAM_MSG | cmd;
+	stream.comp_id = scontrol->comp_id;
+
+	/* send IPC to the DSP */
+	ret = sof_ipc_tx_message(sdev->ipc, stream.hdr.cmd, &stream,
+				 sizeof(stream), &reply, sizeof(reply));
+	if (ret < 0)
+		dev_err(sdev->dev, "error: failed to trigger stream\n");
+
+	return ret;
+}
+
+/* set the active status for playback/capture for the virtual FE */
+static int set_vfe_active_status(struct snd_sof_control *scontrol,
+				 struct snd_soc_card *card, int status)
+{
+	struct snd_soc_pcm_runtime *rtd;
+
+	list_for_each_entry(rtd, &card->rtd_list, list) {
+		if (!strcmp(rtd->dai_link->name, scontrol->vfe_link_name)) {
+
+			/* set playback status */
+			if (rtd->dai_link->dpcm_playback) {
+				rtd->cpu_dai->playback_active = status;
+				rtd->codec_dai->playback_active = status;
+			}
+
+			/* set capture status */
+			if (rtd->dai_link->dpcm_capture) {
+				rtd->cpu_dai->capture_active = status;
+				rtd->codec_dai->capture_active = status;
+			}
+
+			if (status) {
+				/* increment the active count for cpu dai */
+				rtd->cpu_dai->active++;
+			} else {
+				/* decrement the active count for cpu dai */
+				rtd->cpu_dai->active--;
+			}
+		}
+	}
+}
+
+/*
+ * Helper function to send ipc's to trigger siggen pipeline
+ * The siggen pipeline is enabled/disabled only if the
+ * control values change from the old state.
+ */
+static int siggen_pipeline_trigger(struct snd_sof_control *scontrol,
+				   struct snd_sof_dev *sdev,
+				   struct snd_soc_card *card,
+				   int new_state)
+{
+	int ret = 0;
+
+	if (!new_state) {
+
+		/* set runtime status as inactive for virtual FE */
+		set_vfe_active_status(scontrol, card, 0);
+
+		/* free pcm and reset pipeline */
+		ret = siggen_trigger(scontrol, sdev,
+				     SOF_IPC_STREAM_PCM_FREE);
+	} else {
+
+		/* set runtime status as active for virtual FE */
+		set_vfe_active_status(scontrol, card, 1);
+
+		/* enable BE DAI */
+		snd_soc_dpcm_runtime_update(card);
+
+		/* set pcm params */
+		ret = siggen_pcm_params(scontrol, sdev);
+		if (ret < 0)
+			return ret;
+
+		/* enable siggen */
+		ret = siggen_trigger(scontrol, sdev,
+				     SOF_IPC_STREAM_TRIG_START);
+	}
+
+	return ret;
+}
+
 static inline u32 mixer_to_ipc(unsigned int value, u32 *volume_map, int size)
 {
 	if (value >= size)
@@ -246,6 +367,9 @@ int snd_sof_switch_put(struct snd_kcontrol *kcontrol,
 	struct snd_sof_control *scontrol = sm->dobj.private;
 	struct snd_sof_dev *sdev = scontrol->sdev;
 	struct sof_ipc_ctrl_data *cdata = scontrol->control_data;
+	struct snd_soc_dapm_context *dapm =
+		snd_soc_dapm_kcontrol_dapm(kcontrol);
+	struct snd_soc_card *card = dapm->card;
 	unsigned int i, channels = scontrol->num_channels;
 	int ret = 0, new_state, old_state;
 	int changed = 0;
@@ -267,6 +391,52 @@ int snd_sof_switch_put(struct snd_kcontrol *kcontrol,
 			cdata->chanv[i].value = new_state;
 			cdata->chanv[i].channel = i;
 		}
+
+		/*
+		 * notify DSP of switch state update
+		 * if the control values are different
+		 */
+		if (changed)
+			snd_sof_ipc_set_comp_data(sdev->ipc, scontrol,
+						  SOF_IPC_COMP_SET_VALUE,
+						  SOF_CTRL_TYPE_VALUE_CHAN_GET,
+						  SOF_CTRL_CMD_SWITCH);
+		break;
+	case snd_soc_dapm_siggen:
+
+		/*
+		 * A siggen kcontrol is used as an ON/OFF switch,
+		 * so all channel values are assumed to be identical
+		 */
+		for (i = 0; i < channels; i++) {
+			new_state = ucontrol->value.integer.value[0];
+			old_state = cdata->chanv[0].value;
+			if (new_state != old_state)
+				changed = 1;
+			cdata->chanv[i].value = new_state;
+			cdata->chanv[i].channel = i;
+		}
+
+		if (changed) {
+
+			/*
+			 * notify DSP of switch state update
+			 * if the control values are different
+			 */
+			snd_sof_ipc_set_comp_data(sdev->ipc, scontrol,
+						  SOF_IPC_COMP_SET_VALUE,
+						  SOF_CTRL_TYPE_VALUE_CHAN_GET,
+						  SOF_CTRL_CMD_SWITCH);
+
+			/* trigger siggen pipeline */
+			ret = siggen_pipeline_trigger(scontrol, sdev,
+						      card, new_state);
+			if (ret < 0) {
+				dev_err(sdev->dev,
+					"error: triggering siggen pipeline\n");
+				changed = ret;
+			}
+		}
 		break;
 	default:
 
@@ -282,15 +452,19 @@ int snd_sof_switch_put(struct snd_kcontrol *kcontrol,
 			cdata->chanv[i].value = new_state;
 			cdata->chanv[i].channel = i;
 		}
+
+		/*
+		 * notify DSP of switch state update
+		 * if the control values are different
+		 */
+		if (changed)
+			snd_sof_ipc_set_comp_data(sdev->ipc, scontrol,
+						  SOF_IPC_COMP_SET_VALUE,
+						  SOF_CTRL_TYPE_VALUE_CHAN_GET,
+						  SOF_CTRL_CMD_SWITCH);
+
 		break;
 	}
-
-	/* notify DSP of switch state update if the control values are different */
-	if (changed)
-		snd_sof_ipc_set_comp_data(sdev->ipc, scontrol,
-					  SOF_IPC_COMP_SET_VALUE,
-					  SOF_CTRL_TYPE_VALUE_CHAN_GET,
-					  SOF_CTRL_CMD_SWITCH);
 
 	pm_runtime_mark_last_busy(sdev->dev);
 	pm_runtime_put_autosuspend(sdev->dev);
