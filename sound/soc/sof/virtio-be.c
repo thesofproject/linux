ade036acd7436675efbd42c03396ff47d5d6f575
Libin Yang <libin.yang@intel.com>
2018-08-29 19:24:45 +0800

    ASoC: SOF vBE: virtio vq handle
    This patch initialize the virtio vq handling. It dispatches the different
    vq kicks to different handlers. The specific hanlder will be implemented
    later.
    
    Signed-off-by: Libin Yang <libin.yang@intel.com>

--- sound/soc/sof/virtio-be.c
+++ sound/soc/sof/virtio-be.c
@@ -33,9 +33,121 @@
 #include "ops.h"
 #include "virtio-miscdev.h"
 
+/* find client from client ID */
+static struct sof_vbe_client *vbe_client_find(struct snd_sof_dev *sdev,
+					      int client_id)
+{
+	struct sof_vbe_client *client;
+	struct sof_vbe *vbe;
+
+	list_for_each_entry(vbe, &sdev->vbe_list, list) {
+		list_for_each_entry(client, &vbe->client_list, list) {
+			if (client_id == client->vhm_client_id)
+				return client;
+		}
+	}
+
+	return NULL;
+}
+
 /* To be implemented later */
+static void sbe_ipc_fe_not_reply_get(struct sof_vbe *vbe, int vq_idx)
+{}
+
+/* To be implemented later */
+static void sbe_ipc_fe_cmd_get(struct sof_vbe *vbe, int vq_idx)
+{}
+
+static void handle_vq_kick(struct sof_vbe *vbe, int vq_idx)
+{
+	dev_dbg(vbe->sdev->dev, "vq_idx %d\n", vq_idx);
+
+	switch (vq_idx) {
+	case SOF_VIRTIO_IPC_CMD_TX_VQ:
+		/* IPC command from FE to DSP */
+		return sbe_ipc_fe_cmd_get(vbe, vq_idx);
+	case SOF_VIRTIO_IPC_CMD_RX_VQ:
+		/* IPC command reply from DSP to FE - NOT kick */
+		break;
+	case SOF_VIRTIO_IPC_NOT_TX_VQ:
+		/* IPC notification reply from FE to DSP */
+		return sbe_ipc_fe_not_reply_get(vbe, vq_idx);
+	case SOF_VIRTIO_IPC_NOT_RX_VQ:
+		/* IPC notification from DSP to FE - NOT kick */
+		break;
+	default:
+		dev_err(vbe->sdev->dev, "idx %d is invalid\n", vq_idx);
+		break;
+	}
+}
+
 static int handle_kick(int client_id, unsigned long *ioreqs_map)
 {
+	struct vhm_request *req;
+	struct sof_vbe_client *client;
+	struct sof_vbe *vbe;
+	struct snd_sof_dev *sdev = get_sof_dev();
+	int i, handle;
+
+	if (!sdev) {
+		dev_err(sdev->dev, "error: no BE registered for SOF!\n");
+		return -EINVAL;
+	}
+
+	dev_dbg(sdev->dev, "virtio audio kick handling!\n");
+
+	/* get the client this notification is for/from? */
+	client = vbe_client_find(sdev, client_id);
+	if (!client) {
+		dev_err(sdev->dev, "Ooops! client %d not found!\n", client_id);
+		return -EINVAL;
+	}
+	vbe = client->vbe;
+
+	/* go through all vcpu for the valid request buffer */
+	for (i = 0; i < client->max_vcpu; i++) {
+		req = &client->req_buf[i];
+		handle = 0;
+
+		/* is request valid and for this client */
+		if (!req->valid)
+			continue;
+		if (req->client != client->vhm_client_id)
+			continue;
+
+		/* ignore if not processing state */
+		if (req->processed != REQ_STATE_PROCESSING)
+			continue;
+
+		dev_dbg(sdev->dev,
+			"ioreq type %d, direction %d, addr 0x%llx, size 0x%llx, value 0x%x\n",
+			 req->type,
+			 req->reqs.pio_request.direction,
+			 req->reqs.pio_request.address,
+			 req->reqs.pio_request.size,
+			 req->reqs.pio_request.value);
+
+		if (req->reqs.pio_request.direction == REQUEST_READ) {
+			/*
+			 * currently we handle kick only,
+			 * so read will return 0
+			 */
+			req->reqs.pio_request.value = 0;
+		} else {
+			req->reqs.pio_request.value >= 0 ?
+				(handle = 1) : (handle = 0);
+		}
+
+		req->processed = REQ_STATE_SUCCESS;
+
+		// TODO comment what this does.
+		acrn_ioreq_complete_request(client->vhm_client_id, i);
+
+		/* handle VQ kick if needed */
+		if (handle)
+			handle_vq_kick(vbe, req->reqs.pio_request.value);
+	}
+
 	return 0;
 }
 
