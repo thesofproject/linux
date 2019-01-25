0770b3699cb16a104ab4b31e55c5ba10394883dc
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-01-25 16:13:36 -0600

    ASoC: SOF: pci: enable sof_probe_complete callback
    We have no way of knowing if low-level layers probed successfully.
    
    Move all the runtime_pm enablement to the sof_probe_complete callback
    to avoid any possible race conditions.
    
    FIXME: can we have race conditions between the notification and the
    remove? Is a lock necessary?
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/sof/sof-pci-dev.c
+++ sound/soc/sof/sof-pci-dev.c
@@ -150,6 +150,24 @@ static const struct dev_pm_ops sof_pci_pm = {
 			   NULL)
 };
 
+static void sof_pci_probe_complete(struct device *dev)
+{
+	dev_dbg(dev, "Completing SOF PCI probe");
+
+	/* allow runtime_pm */
+	pm_runtime_set_autosuspend_delay(dev, SND_SOF_SUSPEND_DELAY_MS);
+	pm_runtime_use_autosuspend(dev);
+
+	/*
+	 * runtime pm for pci device is "forbidden" by default.
+	 * so call pm_runtime_allow() to enable it.
+	 */
+	pm_runtime_allow(dev);
+
+	/* follow recommendation in pci-driver.c to decrement usage counter */
+	pm_runtime_put_noidle(dev);
+}
+
 static int sof_pci_probe(struct pci_dev *pci,
 			 const struct pci_device_id *pci_id)
 {
@@ -217,25 +235,15 @@ static int sof_pci_probe(struct pci_dev *pci,
 	sof_pdata->dev = &pci->dev;
 	sof_pdata->platform = "sof-audio";
 
+	/* set callback to enable runtime_pm */
+	sof_pdata->sof_probe_complete = sof_pci_probe_complete;
+
 	/* register sof-audio platform driver */
 	ret = sof_create_platform_device(priv);
 	if (ret) {
 		dev_err(dev, "error: failed to create platform device!\n");
 		goto release_regions;
 	}
-
-	/* allow runtime_pm */
-	pm_runtime_set_autosuspend_delay(dev, SND_SOF_SUSPEND_DELAY_MS);
-	pm_runtime_use_autosuspend(dev);
-
-	/*
-	 * runtime pm for pci device is "forbidden" by default.
-	 * so call pm_runtime_allow() to enable it.
-	 */
-	pm_runtime_allow(dev);
-
-	/* follow recommendation in pci-driver.c to decrement usage counter */
-	pm_runtime_put_noidle(dev);
 
 	return ret;
 
