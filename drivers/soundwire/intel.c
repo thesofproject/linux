e0959c1c7bb72328ba33a077cb4561e7736b6bfe
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-07-25 12:16:58 -0500

    soundwire: intel: add delay on restart for enumeration
    We have a conceptual issue on restart: the interaction with the slaves
    can start before (re) enumeration is complete. Add a delay for now but
    we will need to have an async notification that all devices are back
    on the bus.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/intel.c
+++ drivers/soundwire/intel.c
@@ -1323,6 +1323,11 @@ static int intel_resume(struct device *dev)
 		return ret;
 	}
 
+	/* add delay to let Slaves re-enumerate */
+	usleep_range(20000, 30000);
+
+	dev_dbg(dev, "%s done\n", __func__);
+
 	return ret;
 }
 
