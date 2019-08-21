d0de16ff7b0ec0e6689d40b7db10eeed2ba6ceec
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-08-21 15:31:58 +0900

    ASoC: soc-core: rename soc_link_init() to soc_init_pcm_runtime()
    soc-core is using soc_link_init(). It initialize pcm_runtime.
    The purpose is initializing pcm_runtime, not dai_link.
    This patch renames function name, and moves it next to
    snd_soc_add_pcm_runtime().
    
    soc_link_init() sounds like dai_link function,
    but it is for pcm_runtime.
    This patch rename soc_link_init() to soc_init_pcm_runtime().
    
    This patch checks missing return value.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1109,8 +1109,8 @@ static int soc_dai_pcm_new(struct snd_soc_dai **dais, int num_dais,
 	return 0;
 }
 
-static int soc_link_init(struct snd_soc_card *card,
-			 struct snd_soc_pcm_runtime *rtd)
+static int soc_init_pcm_runtime(struct snd_soc_card *card,
+				struct snd_soc_pcm_runtime *rtd)
 {
 	struct snd_soc_dai_link *dai_link = rtd->dai_link;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
@@ -1978,8 +1978,11 @@ static int snd_soc_bind_card(struct snd_soc_card *card)
 		goto probe_end;
 	}
 
-	for_each_card_rtds(card, rtd)
-		soc_link_init(card, rtd);
+	for_each_card_rtds(card, rtd) {
+		ret = soc_init_pcm_runtime(card, rtd);
+		if (ret < 0)
+			goto probe_end;
+	}
 
 	snd_soc_dapm_link_dai_widgets(card);
 	snd_soc_dapm_connect_dai_link_widgets(card);
