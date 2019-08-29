cac84cb91f8c0328f9414c9e15b386b16ffb4151
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-08-29 14:42:29 +0900

    ASoC: soc-link: move soc_rtd_xxx()
    dai_link related function should be implemented at
    soc-link.c.
    This patch moves soc-pcm soc_rtd_xxx()
    to soc-link as snd_soc_link_xxx()
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/soc-link.c
+++ sound/soc/soc-link.c
@@ -28,3 +28,82 @@ int snd_soc_link_init(struct snd_soc_pcm_runtime *rtd)
 
 	return 0;
 }
+
+int snd_soc_link_startup(struct snd_soc_pcm_runtime *rtd,
+			 struct snd_pcm_substream *substream)
+{
+	if (rtd->dai_link->ops &&
+	    rtd->dai_link->ops->startup) {
+		int ret = rtd->dai_link->ops->startup(substream);
+		if (ret < 0)
+			return soc_link_err(rtd, ret);
+	}
+
+	rtd->started = 1;
+
+	return 0;
+}
+
+void snd_soc_link_shutdown(struct snd_soc_pcm_runtime *rtd,
+			   struct snd_pcm_substream *substream)
+{
+	if (rtd->started &&
+	    rtd->dai_link->ops &&
+	    rtd->dai_link->ops->shutdown)
+		rtd->dai_link->ops->shutdown(substream);
+
+	rtd->started = 0;
+}
+
+int snd_soc_link_prepare(struct snd_soc_pcm_runtime *rtd,
+			 struct snd_pcm_substream *substream)
+{
+	if (rtd->dai_link->ops &&
+	    rtd->dai_link->ops->prepare) {
+		int ret = rtd->dai_link->ops->prepare(substream);
+		if (ret < 0)
+			return soc_link_err(rtd, ret);
+	}
+
+	return 0;
+}
+
+int snd_soc_link_hw_params(struct snd_soc_pcm_runtime *rtd,
+			   struct snd_pcm_substream *substream,
+			   struct snd_pcm_hw_params *params)
+{
+	if (rtd->dai_link->ops &&
+	    rtd->dai_link->ops->hw_params) {
+		int ret = rtd->dai_link->ops->hw_params(substream, params);
+		if (ret < 0)
+			return soc_link_err(rtd, ret);
+	}
+
+	rtd->hw_paramed = 1;
+
+	return 0;
+}
+
+void snd_soc_link_hw_free(struct snd_soc_pcm_runtime *rtd,
+			  struct snd_pcm_substream *substream)
+{
+	if (rtd->hw_paramed &&
+	    rtd->dai_link->ops &&
+	    rtd->dai_link->ops->hw_free)
+		rtd->dai_link->ops->hw_free(substream);
+
+	rtd->hw_paramed = 0;
+}
+
+int snd_soc_link_trigger(struct snd_soc_pcm_runtime *rtd,
+			 struct snd_pcm_substream *substream, int cmd)
+{
+	if (rtd->dai_link->ops &&
+	    rtd->dai_link->ops->trigger) {
+		int ret = rtd->dai_link->ops->trigger(substream, cmd);
+		if (ret < 0)
+			return soc_link_err(rtd, ret);
+	}
+
+	return 0;
+}
