5e82ccb0734ab21b3336e2f0fed0505b4413aaf1
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-09-02 12:57:44 +0900

    ASoC: soc-dai: add snd_soc_dai_compr_ack()
    dai related function should be implemented at soc-dai.c.
    This patch adds snd_soc_dai_compr_ack().
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/soc-dai.c
+++ sound/soc/soc-dai.c
@@ -562,3 +562,18 @@ int snd_soc_dai_compr_get_params(struct snd_soc_dai *dai,
 	return 0;
 }
 EXPORT_SYMBOL_GPL(snd_soc_dai_compr_get_params);
+
+int snd_soc_dai_compr_ack(struct snd_soc_dai *dai,
+			  struct snd_compr_stream *cstream,
+			  size_t bytes)
+{
+	if (dai->driver->cops &&
+	    dai->driver->cops->ack) {
+		int ret = dai->driver->cops->ack(cstream, bytes, dai);
+		if (ret < 0)
+			return soc_dai_err(dai, ret);
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(snd_soc_dai_compr_ack);
