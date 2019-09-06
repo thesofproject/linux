3ab533ec3c8d0adde71b158faa005108f93d1f9a
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-09-06 14:30:38 -0500

    ASoC: rt700: move all pm_runtime last, and don't use put_sync_autosuspend
    group all pm_runtime parts last to avoid any issues, and let
    pm_runtime autosuspend later as needed, instead of forcing a
    transition.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/codecs/rt700.c
+++ sound/soc/codecs/rt700.c
@@ -1190,11 +1190,6 @@ int rt700_io_init(struct device *dev, struct sdw_slave *slave)
 	if (rt700->hw_init)
 		return 0;
 
-	/* Enable Runtime PM */
-	pm_runtime_set_autosuspend_delay(&slave->dev, 3000);
-	pm_runtime_use_autosuspend(&slave->dev);
-	pm_runtime_enable(&slave->dev);
-
 	/* reset */
 	regmap_write(rt700->regmap, 0xff01, 0x00);
 	regmap_write(rt700->regmap, 0x7520, 0x00);
@@ -1241,8 +1236,6 @@ int rt700_io_init(struct device *dev, struct sdw_slave *slave)
 	/* Finish Initial Settings, set power to D3 */
 	regmap_write(rt700->regmap, RT700_SET_AUDIO_POWER_STATE, AC_PWRST_D3);
 
-	pm_runtime_put_sync_autosuspend(&slave->dev);
-
 	INIT_DELAYED_WORK(&rt700->jack_detect_work,
 			rt700_jack_detect_handler);
 	INIT_DELAYED_WORK(&rt700->jack_btn_check_work,
@@ -1250,6 +1243,12 @@ int rt700_io_init(struct device *dev, struct sdw_slave *slave)
 
 	/* Mark Slave initialization complete */
 	rt700->hw_init = true;
+
+	/* Enable Runtime PM */
+	pm_runtime_set_autosuspend_delay(&slave->dev, 3000);
+	pm_runtime_use_autosuspend(&slave->dev);
+	pm_runtime_mark_last_busy(&slave->dev);
+	pm_runtime_enable(&slave->dev);
 
 	dev_dbg(&slave->dev, "%s hw_init complete\n", __func__);
 
