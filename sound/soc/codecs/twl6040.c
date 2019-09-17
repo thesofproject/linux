d46ee7d1f5394d06aebba124a83c8227ffcabfd3
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-09-17 13:01:53 +0900

    ASoC: twl6040: merge .digital_mute() into .mute_stream()
    snd_soc_dai_digital_mute() is internally using both
    mute_stream() (1) or digital_mute() (2), but the difference between
    these 2 are only handling direction.
    We can merge digital_mute() into mute_stream
    
    	int snd_soc_dai_digital_mute(xxx, int direction)
    	{
    		...
    		else if (dai->driver->ops->mute_stream)
    (1)			return dai->driver->ops->mute_stream(xxx, direction);
    		else if (direction == SNDRV_PCM_STREAM_PLAYBACK &&
    			 dai->driver->ops->digital_mute)
    (2)			return dai->driver->ops->digital_mute(xxx);
    		...
    	}
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/codecs/twl6040.c
+++ sound/soc/codecs/twl6040.c
@@ -997,8 +997,11 @@ static void twl6040_mute_path(struct snd_soc_component *component, enum twl6040_
 	}
 }
 
-static int twl6040_digital_mute(struct snd_soc_dai *dai, int mute)
+static int twl6040_mute(struct snd_soc_dai *dai, int mute, int direction)
 {
+	if (direction != SNDRV_PCM_STREAM_PLAYBACK)
+		return 0;
+
 	switch (dai->id) {
 	case TWL6040_DAI_LEGACY:
 		twl6040_mute_path(dai->component, TWL6040_DAI_DL1, mute);
@@ -1020,7 +1023,7 @@ static const struct snd_soc_dai_ops twl6040_dai_ops = {
 	.hw_params	= twl6040_hw_params,
 	.prepare	= twl6040_prepare,
 	.set_sysclk	= twl6040_set_dai_sysclk,
-	.digital_mute	= twl6040_digital_mute,
+	.mute_stream	= twl6040_mute,
 };
 
 static struct snd_soc_dai_driver twl6040_dai[] = {
