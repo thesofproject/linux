4cfcda5e0faf8b553ac7cb47feb80c59188a4df5
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-09-17 13:28:32 +0900

    ASoC: cs4265: merge .digital_mute() into .mute_stream()
    snd_soc_dai_digital_mute() is internally using both
    mute_stream() (1) or digital_mute() (2), but the difference between
    these 2 are only handling direction.
    We can merge digital_mute() into mute_stream
    
    	int snd_soc_dai_digital_mute(xxx, int direction)
    	{
    		...
    		else if (dai->driver->ops->mute_stream)
    (1)			return dai->driver->ops->mute_stream(xxx, direction);
    		else if (direction == SNDRV_PCM_STREAM_PLAYBACK &&
    			 dai->driver->ops->digital_mute)
    (2)			return dai->driver->ops->digital_mute(xxx);
    		...
    	}
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/codecs/cs4265.c
+++ sound/soc/codecs/cs4265.c
@@ -378,9 +378,12 @@ static int cs4265_set_fmt(struct snd_soc_dai *codec_dai, unsigned int fmt)
 	return 0;
 }
 
-static int cs4265_digital_mute(struct snd_soc_dai *dai, int mute)
+static int cs4265_mute(struct snd_soc_dai *dai, int mute, int direction)
 {
 	struct snd_soc_component *component = dai->component;
+
+	if (direction != SNDRV_PCM_STREAM_PLAYBACK)
+		return 0;
 
 	if (mute) {
 		snd_soc_component_update_bits(component, CS4265_DAC_CTL,
@@ -498,7 +501,7 @@ static int cs4265_set_bias_level(struct snd_soc_component *component,
 
 static const struct snd_soc_dai_ops cs4265_ops = {
 	.hw_params	= cs4265_pcm_hw_params,
-	.digital_mute	= cs4265_digital_mute,
+	.mute_stream	= cs4265_mute,
 	.set_fmt	= cs4265_set_fmt,
 	.set_sysclk	= cs4265_set_sysclk,
 };
