249aefddd210148d623f127ccdc5d04091da3523
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-09-17 13:03:36 +0900

    ASoC: tlv320aic31xx: merge .digital_mute() into .mute_stream()
    snd_soc_dai_digital_mute() is internally using both
    mute_stream() (1) or digital_mute() (2), but the difference between
    these 2 are only handling direction.
    We can merge digital_mute() into mute_stream
    
    	int snd_soc_dai_digital_mute(xxx, int direction)
    	{
    		...
    		else if (dai->driver->ops->mute_stream)
    (1)			return dai->driver->ops->mute_stream(xxx, direction);
    		else if (direction == SNDRV_PCM_STREAM_PLAYBACK &&
    			 dai->driver->ops->digital_mute)
    (2)			return dai->driver->ops->digital_mute(xxx);
    		...
    	}
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/codecs/tlv320aic31xx.c
+++ sound/soc/codecs/tlv320aic31xx.c
@@ -935,9 +935,13 @@ static int aic31xx_hw_params(struct snd_pcm_substream *substream,
 	return aic31xx_setup_pll(component, params);
 }
 
-static int aic31xx_dac_mute(struct snd_soc_dai *codec_dai, int mute)
+static int aic31xx_dac_mute(struct snd_soc_dai *codec_dai, int mute,
+			    int direction)
 {
 	struct snd_soc_component *component = codec_dai->component;
+
+	if (direction != SNDRV_PCM_STREAM_PLAYBACK)
+		return 0;
 
 	if (mute) {
 		snd_soc_component_update_bits(component, AIC31XX_DACMUTE,
@@ -1336,7 +1340,7 @@ static const struct snd_soc_dai_ops aic31xx_dai_ops = {
 	.hw_params	= aic31xx_hw_params,
 	.set_sysclk	= aic31xx_set_dai_sysclk,
 	.set_fmt	= aic31xx_set_dai_fmt,
-	.digital_mute	= aic31xx_dac_mute,
+	.mute_stream	= aic31xx_dac_mute,
 };
 
 static struct snd_soc_dai_driver dac31xx_dai_driver[] = {
