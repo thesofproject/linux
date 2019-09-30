f6862fa7759dd99e1f6b8ff486b2b436f5b7575d
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2019-09-30 12:49:19 +0200

    ASoC: SOF: IPC: fix error handling
    If an IPC response contains an error code, propagate it to the
    caller, also don't copy the data, if the IPC failed.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- sound/soc/sof/ipc.c
+++ sound/soc/sof/ipc.c
@@ -211,22 +211,34 @@ static int tx_wait_done(struct snd_sof_ipc *ipc, struct snd_sof_ipc_msg *msg,
 				 msecs_to_jiffies(IPC_TIMEOUT_MS));
 
 	if (ret == 0) {
+		struct sof_ipc_reply *reply = reply_data;
+
 		dev_err(sdev->dev, "error: ipc timed out for 0x%x size %d\n",
 			hdr->cmd, hdr->size);
-		snd_sof_dsp_dbg_dump(ipc->sdev, SOF_DBG_REGS | SOF_DBG_MBOX);
-		snd_sof_ipc_dump(ipc->sdev);
-		snd_sof_trace_notify_for_error(ipc->sdev);
-		ret = -ETIMEDOUT;
+		if (!msg->reply_error) {
+			snd_sof_dsp_dbg_dump(ipc->sdev, SOF_DBG_REGS | SOF_DBG_MBOX);
+			snd_sof_ipc_dump(ipc->sdev);
+			snd_sof_trace_notify_for_error(ipc->sdev);
+			ret = -ETIMEDOUT;
+		} else {
+			ret = msg->reply_error;
+		}
+
+		reply->error = ret;
 	} else {
 		/* copy the data returned from DSP */
 		ret = msg->reply_error;
-		if (msg->reply_size)
-			memcpy(reply_data, msg->reply_data, msg->reply_size);
-		if (ret < 0)
-			dev_err(sdev->dev, "error: ipc error for 0x%x size %zu\n",
-				hdr->cmd, msg->reply_size);
-		else
+		if (ret < 0) {
+			dev_err(sdev->dev,
+				"error: ipc error %d for 0x%x size %zu\n",
+				ret, hdr->cmd, msg->reply_size);
+		} else {
+			if (msg->reply_size)
+				memcpy(reply_data, msg->reply_data,
+				       msg->reply_size);
+
 			ipc_log_header(sdev->dev, "ipc tx succeeded", hdr->cmd);
+		}
 	}
 
 	return ret;
