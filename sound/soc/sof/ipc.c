8d05bdd0a066836402e07f3babea0452aff8a931
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2019-09-30 12:54:53 +0200

    ASoC: SOF: IPC: eliminate a memcpy()
    Instead of using a local object for a stream position update IPC and
    then copying it to the context, use the target buffer directly.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- sound/soc/sof/ipc.c
+++ sound/soc/sof/ipc.c
@@ -430,7 +430,7 @@ static void ipc_trace_message(struct snd_sof_dev *sdev, u32 msg_id)
 static void ipc_period_elapsed(struct snd_sof_dev *sdev, u32 msg_id)
 {
 	struct snd_sof_pcm_stream *stream;
-	struct sof_ipc_stream_posn posn;
+	struct sof_ipc_stream_posn *posn;
 	struct snd_sof_pcm *spcm;
 	int direction;
 
@@ -443,12 +443,12 @@ static void ipc_period_elapsed(struct snd_sof_dev *sdev, u32 msg_id)
 	}
 
 	stream = &spcm->stream[direction];
-	snd_sof_ipc_msg_data(sdev, stream->substream, &posn, sizeof(posn));
+	posn = &stream->posn;
+	snd_sof_ipc_msg_data(sdev, stream->substream, posn, sizeof(*posn));
 
 	dev_dbg(sdev->dev, "posn : host 0x%llx dai 0x%llx wall 0x%llx\n",
-		posn.host_posn, posn.dai_posn, posn.wallclock);
+		posn->host_posn, posn->dai_posn, posn->wallclock);
 
-	memcpy(&stream->posn, &posn, sizeof(posn));
 
 	/* only inform ALSA for period_wakeup mode */
 	if (!stream->substream->runtime->no_period_wakeup)
