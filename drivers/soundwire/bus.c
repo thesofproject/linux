8b2493d4d848e178884928e3814968ec9dbe02d2
Rander Wang <rander.wang@intel.com>
2019-10-29 14:34:46 +0800

    soundwire: fix jack detection failure when booting up
    There is a race condition between codec probe and jack status update,
    which prevents the jack status from being reported if it is already
    inserted when the bus starts.
    
    We need to wait until the codec is probed before checking for the
    slave->ops, which are initialized in the codec probe routine.
    
    Signed-off-by: Rander Wang <rander.wang@intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/bus.c
+++ drivers/soundwire/bus.c
@@ -1021,9 +1021,6 @@ static int sdw_update_slave_status(struct sdw_slave *slave,
 {
 	unsigned long time;
 
-	if (!slave->ops || !slave->ops->update_status)
-		return 0;
-
 	if (!slave->probed) {
 		/*
 		 * the slave status update is typically handled in an
@@ -1040,6 +1037,10 @@ static int sdw_update_slave_status(struct sdw_slave *slave,
 			return -ETIMEDOUT;
 		}
 	}
+
+	if (!slave->ops || !slave->ops->update_status)
+		return 0;
+
 	return slave->ops->update_status(slave, status);
 }
 
