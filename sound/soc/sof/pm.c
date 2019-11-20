911388b4c1425be4c37e7a5f78fdaa7755434cf3
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-11-20 11:07:35 -0800

    ASoC: SOF: pm: Fix checks for platform-specific callbacks
    Fix the checks for the platform-specific DSP PM ops
    in the sof_suspend/sof_resume functions.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- sound/soc/sof/pm.c
+++ sound/soc/sof/pm.c
@@ -53,8 +53,12 @@ static int sof_resume(struct device *dev, bool runtime_resume)
 	int ret;
 
 	/* do nothing if dsp resume callbacks are not set */
-	if (!sof_ops(sdev)->resume || !sof_ops(sdev)->runtime_resume)
-		return 0;
+	if (runtime_resume)
+		if (!sof_ops(sdev)->runtime_resume)
+			return 0;
+	else
+		if (!sof_ops(sdev)->resume)
+			return 0;
 
 	/*
 	 * if the runtime_resume flag is set, call the runtime_resume routine
@@ -124,9 +128,13 @@ static int sof_suspend(struct device *dev, bool runtime_suspend)
 	struct snd_sof_dev *sdev = dev_get_drvdata(dev);
 	int ret;
 
-	/* do nothing if dsp suspend callback is not set */
-	if (!sof_ops(sdev)->suspend)
-		return 0;
+	/* do nothing if dsp suspend callbacks are not set */
+	if (runtime_suspend)
+		if (!sof_ops(sdev)->runtime_suspend)
+			return 0;
+	else
+		if (!sof_ops(sdev)->suspend)
+			return 0;
 
 	/* release trace */
 	snd_sof_release_trace(sdev);
