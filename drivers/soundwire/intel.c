1ed8412f1416dbba9d888d3918188001c7759fcc
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-12-19 19:07:12 -0600

    soundwire: intel: add traces to track clock restart mode
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/intel.c
+++ drivers/soundwire/intel.c
@@ -1777,6 +1777,9 @@ static int intel_resume_runtime(struct device *dev)
 	clock_stop_quirks = sdw->link_res->clock_stop_quirks;
 
 	if (clock_stop_quirks & SDW_INTEL_CLK_STOP_TEARDOWN) {
+
+		dev_dbg(dev, "%s resume from TEARDOWN\n", __func__);
+
 		ret = intel_init(sdw);
 		if (ret) {
 			dev_err(dev, "%s failed: %d", __func__, ret);
@@ -1802,6 +1805,9 @@ static int intel_resume_runtime(struct device *dev)
 			return ret;
 		}
 	} else if (clock_stop_quirks & SDW_INTEL_CLK_STOP_BUS_RESET) {
+
+		dev_dbg(dev, "%s resume from BUS_RESET\n", __func__);
+
 		ret = intel_init(sdw);
 		if (ret) {
 			dev_err(dev, "%s failed: %d", __func__, ret);
@@ -1839,6 +1845,9 @@ static int intel_resume_runtime(struct device *dev)
 		}
 
 	} else if (!clock_stop_quirks) {
+
+		dev_err(dev, "%s resume with no quirks\n", __func__);
+
 		ret = intel_init(sdw);
 		if (ret) {
 			dev_err(dev, "%s failed: %d", __func__, ret);
