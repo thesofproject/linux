58fcfe71dc167c2d4c090a360ebf39357016b8fb
Adrian Bonislawski <adrian.bonislawski@linux.intel.com>
2020-01-20 09:55:20 +0100

    ASoC: SOF: increase ipc msg max size
    Increased ipc msg max size will allow to fit
    a bigger ipc, e.g. needed for mux channel map.
    
    Signed-off-by: Adrian Bonislawski <adrian.bonislawski@linux.intel.com>

--- include/sound/sof/header.h
+++ include/sound/sof/header.h
@@ -114,7 +114,7 @@
 #define SOF_IPC_MESSAGE_ID(x)			((x) & 0xffff)
 
 /* maximum message size for mailbox Tx/Rx */
-#define SOF_IPC_MSG_MAX_SIZE			384
+#define SOF_IPC_MSG_MAX_SIZE			768
 
 /*
  * Structure Header - Header for all IPC structures except command structs.
