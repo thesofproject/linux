daa2132be066fbd87225ef09151055645237ee18
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-03-29 09:30:39 -0500

    clk: hifiberry-dacpro: update SDPX/copyright
    Reformat license information and add Intel copyright
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/clk/clk-hifiberry-dacpro.c
+++ drivers/clk/clk-hifiberry-dacpro.c
@@ -1,17 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
+ * Copyright (c) 2015 Stuart MacLean
+ * Copyright (c) 2020 Intel Corporation
+ *
  * Clock Driver for HiFiBerry DAC Pro
  *
- * Author: Stuart MacLean
- *         Copyright 2015
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
  */
 
 #include <linux/clk-provider.h>
