fbedc3f073906d098924303b1ac05b4dd5431e69
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-07-31 20:21:39 -0500

    ASOC: SOF: Intel: hda-codec: move unused label to correct position
    Cppcheck reports the following warning:
    
    sound/soc/sof/intel/hda-codec.c:191:1: style: Label 'error' is not
    used. [unusedLabel]
    
    This label is indeed only used conditionally, move it where it's
    actually used.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/sof/intel/hda-codec.c
+++ sound/soc/sof/intel/hda-codec.c
@@ -178,6 +178,11 @@ static int hda_codec_probe(struct snd_sof_dev *sdev, int address,
 	}
 
 	return ret;
+
+error:
+	snd_hdac_ext_bus_device_exit(hdev);
+	return -ENOENT;
+
 #else
 	hdev = devm_kzalloc(sdev->dev, sizeof(*hdev), GFP_KERNEL);
 	if (!hdev)
@@ -187,10 +192,6 @@ static int hda_codec_probe(struct snd_sof_dev *sdev, int address,
 
 	return ret;
 #endif
-
-error:
-	snd_hdac_ext_bus_device_exit(hdev);
-	return -ENOENT;
 }
 
 /* Codec initialization */
