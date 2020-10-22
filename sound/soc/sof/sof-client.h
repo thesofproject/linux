c8012d821490f518f225e42dd8f776369690357d
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2020-10-22 16:26:46 -0700

    ASoC: SOF: Introduce IPC SOF client support
    A client in the SOF (Sound Open Firmware) context is a driver that needs
    to communicate with the DSP via IPC messages. The SOF core is responsible
    for serializing the IPC messages to the DSP from the different clients.
    
    One example of an SOF client would be an IPC test client that floods the
    DSP with test IPC messages to validate if the serialization works as
    expected.
    
    Multi-client support will also add the ability to split the existing audio
    cards into multiple ones, so as to e.g. to deal with HDMI with a dedicated
    client instead of adding HDMI to all cards.
    
    This patch introduces descriptors for SOF client driver and SOF client
    device along with APIs for registering and unregistering a SOF client
    driver, sending IPCs from a client device and accessing the SOF core
    debugfs root entry.
    
    Along with this, add a couple of new members to struct snd_sof_dev that
    will be used for maintaining the list of clients.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Co-developed-by: Fred Oh <fred.oh@linux.intel.com>
    Signed-off-by: Fred Oh <fred.oh@linux.intel.com>
    Signed-off-by: Dave Ertman <david.m.ertman@intel.com>
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

---
+++ sound/soc/sof/sof-client.h
@@ -0,0 +1,47 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef __SOC_SOF_CLIENT_H
+#define __SOC_SOF_CLIENT_H
+
+#include <linux/auxiliary_bus.h>
+#include <linux/device.h>
+#include <linux/list.h>
+
+struct snd_sof_dev;
+struct dentry;
+
+/* SOF client device */
+struct sof_client_dev {
+	struct auxiliary_device auxdev;
+	struct snd_sof_dev *sdev;
+	struct list_head list;	/* item in SOF core client dev list */
+	void *data;
+};
+
+#define sof_client_dev_to_sof_dev(cdev)		(cdev->sdev)
+
+#define auxiliary_dev_to_sof_client_dev(auxiliary_dev) \
+	container_of(auxiliary_dev, struct sof_client_dev, auxdev)
+
+int sof_client_ipc_tx_message(struct sof_client_dev *cdev, void *ipc_msg,
+			      void *reply_data, size_t reply_bytes);
+
+struct dentry *sof_client_get_debugfs_root(struct sof_client_dev *cdev);
+
+#if IS_ENABLED(CONFIG_SND_SOC_SOF_CLIENT)
+int sof_client_dev_register(struct snd_sof_dev *sdev, const char *name, u32 id);
+void sof_client_dev_unregister(struct snd_sof_dev *sdev, const char *name, u32 id);
+#else
+static inline int sof_client_dev_register(struct snd_sof_dev *sdev,
+					  const char *name, u32 id)
+{
+	return 0;
+}
+
+static inline void sof_client_dev_unregister(struct snd_sof_dev *sdev,
+					     const char *name, u32 id)
+{
+}
+#endif
+
+#endif /* __SOC_SOF_CLIENT_H */
