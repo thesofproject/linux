d1356ba4bfa583b8e54b87045ac010fa09804bfb
V sujith kumar Reddy <vsujithkumar.reddy@amd.com>
2021-12-08 06:48:23 +0530

    ASoC: SOF: amd : Implementation of sof host stream pointer callback function
    This patch adds pointer callback implementation for acp pcm driver.
    
    Signed-off-by: V sujith kumar Reddy <vsujithkumar.reddy@amd.com>

--- sound/soc/sof/amd/acp.h
+++ sound/soc/sof/amd/acp.h
@@ -153,6 +153,18 @@ struct acp_dev_data {
 	struct pci_dev *smn_dev;
 };
 
+enum acp_pcm_types {
+	I2S_BT = 0,
+	I2S_SP,
+	PDM_DMIC,
+	PCM_NONE,
+};
+
+struct acp_pcm_table {
+	u8 pcm_index;
+	char *pcm_name;
+};
+
 void memcpy_to_scratch(struct snd_sof_dev *sdev, u32 offset, unsigned int *src, size_t bytes);
 void memcpy_from_scratch(struct snd_sof_dev *sdev, u32 offset, unsigned int *dst, size_t bytes);
 
@@ -203,6 +215,8 @@ int acp_pcm_close(struct snd_sof_dev *sdev, struct snd_pcm_substream *substream)
 int acp_pcm_hw_params(struct snd_sof_dev *sdev, struct snd_pcm_substream *substream,
 		      struct snd_pcm_hw_params *params,
 		      struct snd_sof_platform_stream_params *platform_params);
+snd_pcm_uframes_t acp_pcm_pointer(struct snd_sof_dev *sdev,
+				  struct snd_pcm_substream *substream);
 
 extern struct snd_sof_dsp_ops sof_renoir_ops;
 
