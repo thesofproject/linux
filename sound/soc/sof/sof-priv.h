a677e9ad90cd2f1e92507c7967e64d7139e8d1ff
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2021-12-17 22:58:20 +0200

    ASoC: SOF: loader: Remove the old fw_ready related code
    The fw_ready is handled internally to ipc3, we can remove the old code
    from the loader.c along with the functions only used by the fw_ready()
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

--- sound/soc/sof/sof-priv.h
+++ sound/soc/sof/sof-priv.h
@@ -179,11 +179,6 @@ struct snd_sof_dsp_ops {
 	int (*load_firmware)(struct snd_sof_dev *sof_dev); /* mandatory */
 	int (*load_module)(struct snd_sof_dev *sof_dev,
 			   struct snd_sof_mod_hdr *hdr); /* optional */
-	/*
-	 * FW ready checks for ABI compatibility and creates
-	 * memory windows at first boot
-	 */
-	int (*fw_ready)(struct snd_sof_dev *sdev, u32 msg_id); /* mandatory */
 
 	/* connect pcm substream to a host stream */
 	int (*pcm_open)(struct snd_sof_dev *sdev,
@@ -594,7 +589,6 @@ int sof_ipc_tx_message_no_pm(struct snd_sof_ipc *ipc, void *msg_data, size_t msg
 			     void *reply_data, size_t reply_bytes);
 int sof_ipc_prepare_send_msg(struct snd_sof_dev *sdev,
 			     void *msg_data, size_t msg_bytes, size_t reply_bytes);
-int sof_ipc_init_msg_memory(struct snd_sof_dev *sdev);
 
 static inline void snd_sof_ipc_process_reply(struct snd_sof_dev *sdev, u32 msg_id)
 {
@@ -665,8 +659,6 @@ int sof_block_write(struct snd_sof_dev *sdev, enum snd_sof_fw_blk_type blk_type,
 		    u32 offset, void *src, size_t size);
 int sof_block_read(struct snd_sof_dev *sdev, enum snd_sof_fw_blk_type blk_type,
 		   u32 offset, void *dest, size_t size);
-
-int sof_fw_ready(struct snd_sof_dev *sdev, u32 msg_id);
 
 int sof_ipc_msg_data(struct snd_sof_dev *sdev,
 		     struct snd_pcm_substream *substream,
