376dab0a886f148b4383e33638cff543d639b72a
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2021-12-22 18:14:55 +0200

    ASoC: Intel: boards: add configuration for RTL5682+MX98390
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

--- sound/soc/intel/common/soc-acpi-intel-adl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-adl-match.c
@@ -447,6 +447,14 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_adl_machines[] = {
 		.sof_fw_filename = "sof-adl.ri",
 		.sof_tplg_filename = "sof-adl-max98390-rt5682.tplg",
 	},
+	{
+		.id = "RTL5682",
+		.drv_name = "adl_max98390_rt5682",
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &adl_max98390_amp,
+		.sof_fw_filename = "sof-adl.ri",
+		.sof_tplg_filename = "sof-adl-max98390-rt5682.tplg",
+	},
 	{},
 };
 EXPORT_SYMBOL_GPL(snd_soc_acpi_intel_adl_machines);
