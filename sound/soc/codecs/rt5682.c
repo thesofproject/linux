7e33393408cd3f4c00533660ea6aa77c16cf5b41
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2022-02-07 09:48:57 +0200

    ASoC: rt5682: Clear rt5682->component on component remove
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

--- sound/soc/codecs/rt5682.c
+++ sound/soc/codecs/rt5682.c
@@ -2947,6 +2947,8 @@ static void rt5682_remove(struct snd_soc_component *component)
 	dev_dbg(component->dev, "%s\n", __func__);
 
 	rt5682_reset(rt5682);
+
+	rt5682->component = NULL;
 }
 
 #ifdef CONFIG_PM
