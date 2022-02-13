e932899b4154d35884a143a227fb6c8e3da39be2
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2022-02-13 12:34:02 -0800

    ASoC: SOF: IPC4: Add pcm ops
    Define and set the PCM ops for IPC4.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- sound/soc/sof/ipc4-ops.h
+++ sound/soc/sof/ipc4-ops.h
@@ -42,5 +42,6 @@ extern const struct sof_ipc_ops ipc4_ops;
 extern const struct sof_ipc_fw_loader_ops ipc4_loader_ops;
 extern const struct sof_ipc_tplg_ops ipc4_tplg_ops;
 extern const struct sof_ipc_tplg_control_ops tplg_ipc4_control_ops;
+extern const struct sof_ipc_pcm_ops ipc4_pcm_ops;
 
 #endif
