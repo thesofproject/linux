57b359a67edc8e62be3d797567bbb0474e0dd622
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2022-02-13 01:27:10 -0800

    ASoC: SOF: IPC4: Add FW loader ops
    Define and add the FW loader ops for IPC4. Also, introduce a new
    structure, struct sof_ipc4_private_data that will be used to define some
    IPC4-sepcific data.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- sound/soc/sof/ipc4-ops.h
+++ sound/soc/sof/ipc4-ops.h
@@ -9,6 +9,7 @@
 #ifndef __SOUND_SOC_SOF_IPC4_OPS_H
 #define __SOUND_SOC_SOF_IPC4_OPS_H
 
+#include <sound/sof/ext_manifest4.h>
 #include "sof-priv.h"
 
 /**
@@ -23,6 +24,21 @@ struct sof_ipc4_data {
 	u32 manifest_fw_hdr_offset;
 };
 
+/**
+ * struct sof_ipc4_fw_module - IPC4 module info
+ * @sof_man4_module : Module info
+ * @m_ida: Module instance identifier
+ * @bss_size: Module object size
+ * @private: Module private data
+ */
+struct sof_ipc4_fw_module {
+	struct sof_man4_module man4_module_entry;
+	struct ida m_ida;
+	u32 bss_size;
+	void *private;
+};
+
 extern const struct sof_ipc_ops ipc4_ops;
+extern const struct sof_ipc_fw_loader_ops ipc4_loader_ops;
 
 #endif
