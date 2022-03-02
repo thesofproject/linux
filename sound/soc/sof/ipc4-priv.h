ad563985abec242a812e9bbd97605927e415c4b1
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2022-03-02 10:00:25 +0200

    ASoC: SOF: HDA: Set IPC4-specific DSP ops for CaVS platforms
    Set the DSP ops for IPC4 for APL, CNL and TGL platforms. The FW reports
    data about the modules supported by the base FW in its manifest. So add
    a struct sof_ipc4_data to hold the module data and manifest FW header.
    This structure will be allocated when the ops are initialixed for each
    platform and populated when the FW is loaded. The "private" field in
    struct snd_sof_dev which is unused today with IPC3 is used to save this
    data for IPC4.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

---
+++ sound/soc/sof/ipc4-priv.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-3-Clause) */
+/*
+ * This file is provided under a dual BSD/GPLv2 license.  When using or
+ * redistributing this file, you may do so under either license.
+ *
+ * Copyright(c) 2022 Intel Corporation. All rights reserved.
+ */
+
+#ifndef __SOUND_SOC_SOF_IPC4_PRIV_H
+#define __SOUND_SOC_SOF_IPC4_PRIV_H
+
+#include "sof-priv.h"
+
+/**
+ * struct sof_ipc4_private_data - IPC4-specific data
+ * @num_fw_modules : Number of modules in base FW
+ * @fw_modules: Array of base FW modules
+ * @manifest_fw_hdr_offset: FW header offset in the manifest
+ */
+struct sof_ipc4_data {
+	int num_fw_modules;
+	void *fw_modules;
+	u32 manifest_fw_hdr_offset;
+};
+
+#endif
