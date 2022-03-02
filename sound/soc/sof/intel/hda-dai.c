5a5866904f255abce63dac28cf9a5e1b91b46442
Bard Liao <yung-chuan.liao@linux.intel.com>
2022-03-02 19:26:20 +0800

    ASoC: SOF: Intel: add trigger callback into sdw_callback
    For IPC4, we need to set pipeline state in BE DAI trigger.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- sound/soc/sof/intel/hda-dai.c
+++ sound/soc/sof/intel/hda-dai.c
@@ -610,8 +610,7 @@ static const struct snd_soc_dai_ops ipc3_ssp_dai_ops = {
 	.shutdown = ssp_dai_shutdown,
 };
 
-static int ipc4_be_dai_trigger(struct snd_pcm_substream *substream,
-			       int cmd, struct snd_soc_dai *dai)
+int ipc4_be_dai_trigger(struct snd_pcm_substream *substream, int cmd, struct snd_soc_dai *dai)
 {
 	struct snd_sof_widget *pipe_widget;
 	struct sof_ipc4_pipeline *pipeline;
