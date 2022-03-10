8915f254e1d455c7efbbae221bba8915b2749b37
Bard Liao <yung-chuan.liao@linux.intel.com>
2022-03-10 14:51:07 +0800

    fixup! ASoC: SOF: Intel: add trigger callback into sdw_callback
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- sound/soc/sof/intel/hda-dai.c
+++ sound/soc/sof/intel/hda-dai.c
@@ -610,7 +610,8 @@ static const struct snd_soc_dai_ops ipc3_ssp_dai_ops = {
 	.shutdown = ssp_dai_shutdown,
 };
 
-int ipc4_be_dai_trigger(struct snd_pcm_substream *substream, int cmd, struct snd_soc_dai *dai)
+
+int ipc4_sdw_trigger(struct snd_soc_dai *dai, int cmd, int stream)
 {
 	struct snd_sof_widget *pipe_widget;
 	struct sof_ipc4_pipeline *pipeline;
@@ -619,7 +620,7 @@ int ipc4_be_dai_trigger(struct snd_pcm_substream *substream, int cmd, struct snd
 	struct snd_sof_dev *sdev;
 	int ret;
 
-	w = snd_soc_dai_get_widget(dai, substream->stream);
+	w = snd_soc_dai_get_widget(dai, stream);
 	swidget = w->dobj.private;
 	pipe_widget = swidget->pipe_widget;
 	pipeline = pipe_widget->private;
@@ -652,6 +653,12 @@ int ipc4_be_dai_trigger(struct snd_pcm_substream *substream, int cmd, struct snd
 	}
 
 	return 0;
+}
+
+static int ipc4_be_dai_trigger(struct snd_pcm_substream *substream,
+			       int cmd, struct snd_soc_dai *dai)
+{
+	return ipc4_sdw_trigger(dai, cmd, substream->stream);
 }
 
 static const struct snd_soc_dai_ops ipc4_dmic_dai_ops = {
