94d4835e1aaf93273f082b11e5c781473355e123
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2022-06-16 11:33:48 +0300

    ASoC: SOF: pm: Set S4 as suspend_target in FW_CRASHED / FW_BOOT_FAILED state
    Now that we have definitions of deeper sleep states we can use it to help
    with error recovery.
    If the firmware failed to boot or it is crashed, try to recover at the next
    resume time with a full firmware loading sequence.
    
    The S4 target state will skip the IMR boot on Intel platforms where it is
    supported for example.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

--- sound/soc/sof/pm.c
+++ sound/soc/sof/pm.c
@@ -327,12 +327,14 @@ int snd_sof_prepare(struct device *dev)
 	sdev->system_suspend_target = SOF_SUSPEND_S3;
 
 	/*
-	 * if the firmware is crashed or boot failed then we try to aim for S3
-	 * to reboot the firmware
+	 * if the firmware is crashed or boot failed then we try to aim for S4
+	 * to re-initialize the DSP and force re-load the firmware
 	 */
 	if (sdev->fw_state == SOF_FW_CRASHED ||
-	    sdev->fw_state == SOF_FW_BOOT_FAILED)
+	    sdev->fw_state == SOF_FW_BOOT_FAILED) {
+		sdev->system_suspend_target = SOF_SUSPEND_S4;
 		return 0;
+	}
 
 	if (!desc->use_acpi_target_states)
 		return 0;
