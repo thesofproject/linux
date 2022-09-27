324a86c315cba0bab63586723740c8675f284952
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2022-09-27 12:47:48 +0200

    ASoC: SOF: Intel: hda-codec: add hda_codec_check_rirb_status() helper
    move existing functionality to new helper.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Rander Wang <rander.wang@intel.com>
    Reviewed-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Reviewed-by: Péter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- sound/soc/sof/intel/hda.h
+++ sound/soc/sof/intel/hda.h
@@ -721,6 +721,7 @@ void hda_codec_init_cmd_io(struct snd_sof_dev *sdev);
 void hda_codec_stop_cmd_io(struct snd_sof_dev *sdev);
 void hda_codec_detect_mask(struct snd_sof_dev *sdev);
 void hda_codec_rirb_status_clear(struct snd_sof_dev *sdev);
+bool hda_codec_check_rirb_status(struct snd_sof_dev *sdev);
 void hda_codec_set_codec_wakeup(struct snd_sof_dev *sdev, bool status);
 void hda_codec_device_remove(struct snd_sof_dev *sdev);
 
@@ -734,6 +735,7 @@ static inline void hda_codec_init_cmd_io(struct snd_sof_dev *sdev) { }
 static inline void hda_codec_stop_cmd_io(struct snd_sof_dev *sdev) { }
 static inline void hda_codec_detect_mask(struct snd_sof_dev *sdev) { }
 static inline void hda_codec_rirb_status_clear(struct snd_sof_dev *sdev) { }
+static inline bool hda_codec_check_rirb_status(struct snd_sof_dev *sdev) { return false; }
 static inline void hda_codec_set_codec_wakeup(struct snd_sof_dev *sdev, bool status) { }
 static inline void hda_codec_device_remove(struct snd_sof_dev *sdev) { }
 
