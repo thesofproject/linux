05e23b54901a5673d6498cd2f00ad18e0a5c550f
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2022-10-09 10:08:02 -0700

    ASoC: SOF: sof-audio: skip prepapre/unprepare if swidget is NULL
    Skip preparing/unpreparing widgets if the swidget pointer is NULL. This
    will be true in the case of virtual widgets in topology that were added
    for reusing the legacy HDA machine driver with SOF.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- sound/soc/sof/sof-audio.c
+++ sound/soc/sof/sof-audio.c
@@ -283,7 +283,7 @@ sof_unprepare_widgets_in_path(struct snd_sof_dev *sdev, struct snd_soc_dapm_widg
 	struct snd_soc_dapm_path *p;
 
 	/* return if the widget is in use or if it is already unprepared */
-	if (!swidget->prepared || swidget->use_count > 1)
+	if (!swidget || !swidget->prepared || swidget->use_count > 1)
 		return;
 
 	widget_ops = tplg_ops ? tplg_ops->widget : NULL;
@@ -322,7 +322,7 @@ sof_prepare_widgets_in_path(struct snd_sof_dev *sdev, struct snd_soc_dapm_widget
 	if (!widget_ops)
 		return 0;
 
-	if (!widget_ops[widget->id].ipc_prepare || swidget->prepared)
+	if (!swidget || !widget_ops[widget->id].ipc_prepare || swidget->prepared)
 		goto sink_prepare;
 
 	/* prepare the source widget */
