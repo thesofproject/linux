0c6c0c47ee6a2c3b5bac218c15db5390fae365ea
Bard Liao <yung-chuan.liao@linux.intel.com>
2022-10-21 19:25:03 +0800

    ASoC: SOF: connect widget that is in use
    Normally, we don't connect widgets that are in wrong direction.
    But we do need to connect the widget if it is already in use.
    That's how we connect widgets between playback PCM and capture PCM.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- sound/soc/sof/sof-audio.c
+++ sound/soc/sof/sof-audio.c
@@ -249,7 +249,8 @@ static int sof_setup_pipeline_connections(struct snd_sof_dev *sdev,
 				if (!swidget)
 					continue;
 
-				if (swidget->dir != dir)
+				/* We need to connect the widget that is in use */
+				if (swidget->dir != dir && !swidget->use_count)
 					continue;
 
 				if (p->sink->dobj.private) {
@@ -272,7 +273,8 @@ static int sof_setup_pipeline_connections(struct snd_sof_dev *sdev,
 				if (!swidget)
 					continue;
 
-				if (swidget->dir != dir)
+				/* We need to connect the widget that is in use */
+				if (swidget->dir != dir && !swidget->use_count)
 					continue;
 
 				if (p->source->dobj.private) {
