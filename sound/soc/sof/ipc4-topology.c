0aeecef57c7d19f2ea0137617a4fad36e7110347
Chao Song <chao.song@linux.intel.com>
2023-01-19 11:56:15 +0800

    ASoC: SOF: ipc4-topology: update output hw_params in process prepare
    Some process modules output hw_params is different from its input
    hw_params. Let's add a common help function to support this feature.
    And update the hw_params in process module prepare callback.
    
    Signed-off-by: Libin Yang <libin.yang@intel.com>
    Signed-off-by: Chao Song <chao.song@linux.intel.com>

--- sound/soc/sof/ipc4-topology.c
+++ sound/soc/sof/ipc4-topology.c
@@ -1076,6 +1076,51 @@ static int sof_ipc4_widget_assign_instance_id(struct snd_sof_dev *sdev,
 	return 0;
 }
 
+/* update hw_params based on the audio stream format */
+static int sof_ipc4_update_hw_params(struct snd_sof_dev *sdev, struct snd_pcm_hw_params *params,
+				     struct sof_ipc4_audio_format *fmt)
+{
+	struct snd_interval *i;
+	struct snd_mask *m;
+	snd_pcm_format_t snd_fmt;
+	unsigned int channels, rate;
+	int valid_bits;
+
+	/* update rate */
+	rate = fmt->sampling_frequency;
+	i = hw_param_interval(params, SNDRV_PCM_HW_PARAM_RATE);
+	i->min = rate;
+	i->max = rate;
+
+	/* update channel */
+	channels = SOF_IPC4_AUDIO_FORMAT_CFG_CHANNELS_COUNT(fmt->fmt_cfg);
+	i = hw_param_interval(params, SNDRV_PCM_HW_PARAM_CHANNELS);
+	i->min = channels;
+	i->max = channels;
+
+	/* update fmt */
+	valid_bits = SOF_IPC4_AUDIO_FORMAT_CFG_V_BIT_DEPTH(fmt->fmt_cfg);
+	switch (valid_bits) {
+	case 16:
+		snd_fmt = SNDRV_PCM_FORMAT_S16_LE;
+		break;
+	case 24:
+		snd_fmt = SNDRV_PCM_FORMAT_S24_LE;
+		break;
+	case 32:
+		snd_fmt = SNDRV_PCM_FORMAT_S32_LE;
+		break;
+	default:
+		dev_err(sdev->dev, "invalid pcm valid_bits %d\n", valid_bits);
+		return -EINVAL;
+	}
+	m = hw_param_mask(params, SNDRV_PCM_HW_PARAM_FORMAT);
+	snd_mask_none(m);
+	snd_mask_set_format(m, snd_fmt);
+
+	return 0;
+}
+
 static int sof_ipc4_init_audio_fmt(struct snd_sof_dev *sdev,
 				   struct snd_sof_widget *swidget,
 				   struct sof_ipc4_base_module_cfg *base_config,
@@ -1748,6 +1793,13 @@ static int sof_ipc4_prepare_process_module(struct snd_sof_widget *swidget,
 	control_data = process->cdata;
 	if (control_data)
 		memcpy(cfg, control_data->data->data, control_data->data->size);
+
+	if (process->init_payload_format == INIT_PAYLOAD_WITH_OUTPUT_FMT) {
+		ret = sof_ipc4_update_hw_params(sdev, pipeline_params,
+				&process->output_format);
+		if (ret)
+			return ret;
+	}
 
 	return 0;
 }
