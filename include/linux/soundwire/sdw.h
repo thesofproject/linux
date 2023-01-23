63155e43c85625be72dc41e942020a12f586b875
Richard Fitzgerald <rf@opensource.cirrus.com>
2023-01-23 16:49:49 +0000

    soundwire: bus: Remove unused reset_page_addr() callback
    A previous patch removed unnecessary zeroing of the page registers
    after a paged transaction, so now the reset_page_addr callback is
    unused and can be removed.
    
    Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20230123164949.245898-3-rf@opensource.cirrus.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    (cherry picked from commit 3bd3bc2ada84229e74f974d4dafcaca59ae8347f)

--- include/linux/soundwire/sdw.h
+++ include/linux/soundwire/sdw.h
@@ -839,7 +839,6 @@ struct sdw_defer {
  * @xfer_msg: Transfer message callback
  * @xfer_msg_defer: Defer version of transfer message callback. The message is handled with the
  * bus struct @sdw_defer
- * @reset_page_addr: Reset the SCP page address registers
  * @set_bus_conf: Set the bus configuration
  * @pre_bank_switch: Callback for pre bank switch
  * @post_bank_switch: Callback for post bank switch
@@ -854,8 +853,6 @@ struct sdw_master_ops {
 			(struct sdw_bus *bus, struct sdw_msg *msg);
 	enum sdw_command_response (*xfer_msg_defer)
 			(struct sdw_bus *bus);
-	enum sdw_command_response (*reset_page_addr)
-			(struct sdw_bus *bus, unsigned int dev_num);
 	int (*set_bus_conf)(struct sdw_bus *bus,
 			struct sdw_bus_params *params);
 	int (*pre_bank_switch)(struct sdw_bus *bus);
