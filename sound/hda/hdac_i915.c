61f43d121099eedd9264e804f725f9d022189a56
Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
2023-07-18 10:45:22 +0200

    ALSA: hda/i915: Remove extra argument from snd_hdac_i915_init
    Now that all drivers have moved from modprobe loading to
    handling -EPROBE_DEFER, we can remove the argument again.
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>

--- sound/hda/hdac_i915.c
+++ sound/hda/hdac_i915.c
@@ -152,7 +152,7 @@ static int i915_gfx_present(struct pci_dev *hdac_pci)
  *
  * Returns zero for success or a negative error code.
  */
-int snd_hdac_i915_init(struct hdac_bus *bus, bool allow_modprobe)
+int snd_hdac_i915_init(struct hdac_bus *bus)
 {
 	struct drm_audio_component *acomp;
 	int err;
@@ -168,21 +168,10 @@ int snd_hdac_i915_init(struct hdac_bus *bus, bool allow_modprobe)
 	acomp = bus->audio_component;
 	if (!acomp)
 		return -ENODEV;
-	if (allow_modprobe && !acomp->ops) {
-		if (!IS_ENABLED(CONFIG_MODULES) ||
-		    !request_module("i915")) {
-			/* 60s timeout */
-			wait_for_completion_killable_timeout(&acomp->master_bind_complete,
-							     msecs_to_jiffies(60 * 1000));
-		}
-	}
 	if (!acomp->ops) {
-		if (allow_modprobe)
-			dev_info(bus->dev, "couldn't bind with audio component\n");
-		else
-			dev_dbg(bus->dev, "couldn't bind with audio component\n");
+		dev_dbg(bus->dev, "couldn't bind with audio component\n");
 		snd_hdac_acomp_exit(bus);
-		return allow_modprobe ? -ENODEV : -EPROBE_DEFER;
+		return -EPROBE_DEFER;
 	}
 	return 0;
 }
