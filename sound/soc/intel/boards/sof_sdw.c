bbb21240c78a38e49be5fb0795a7e8e16550b6ce
Vijendar Mukunda <Vijendar.Mukunda@amd.com>
2024-04-16 12:33:18 +0530

    ASoC: intel/sdw_utils: rename ignore_pch_dmic variable name
    Rename 'ignore_pch_dmic' variable name as 'ignore_internal_dmic' to make
    it more generic across different platforms to represent internal dmic.
    
    Signed-off-by: Vijendar Mukunda <Vijendar.Mukunda@amd.com>

--- sound/soc/intel/boards/sof_sdw.c
+++ sound/soc/intel/boards/sof_sdw.c
@@ -716,7 +716,7 @@ static struct sof_sdw_codec_info codec_info_list[] = {
 	{
 		.part_id = 0x714,
 		.version_id = 3,
-		.ignore_pch_dmic = true,
+		.ignore_internal_dmic = true,
 		.dais = {
 			{
 				.direction = {false, true},
@@ -731,7 +731,7 @@ static struct sof_sdw_codec_info codec_info_list[] = {
 	{
 		.part_id = 0x715,
 		.version_id = 3,
-		.ignore_pch_dmic = true,
+		.ignore_internal_dmic = true,
 		.dais = {
 			{
 				.direction = {false, true},
@@ -746,7 +746,7 @@ static struct sof_sdw_codec_info codec_info_list[] = {
 	{
 		.part_id = 0x714,
 		.version_id = 2,
-		.ignore_pch_dmic = true,
+		.ignore_internal_dmic = true,
 		.dais = {
 			{
 				.direction = {false, true},
@@ -761,7 +761,7 @@ static struct sof_sdw_codec_info codec_info_list[] = {
 	{
 		.part_id = 0x715,
 		.version_id = 2,
-		.ignore_pch_dmic = true,
+		.ignore_internal_dmic = true,
 		.dais = {
 			{
 				.direction = {false, true},
@@ -1257,7 +1257,7 @@ static int parse_sdw_endpoints(struct snd_soc_card *card,
 			if (!codec_info)
 				return -EINVAL;
 
-			ctx->ignore_pch_dmic |= codec_info->ignore_pch_dmic;
+			ctx->ignore_internal_dmic |= codec_info->ignore_internal_dmic;
 
 			codec_name = get_codec_name(dev, codec_info, adr_link, i);
 			if (!codec_name)
@@ -1733,7 +1733,7 @@ static int sof_card_dai_links_create(struct snd_soc_card *card)
 
 	/* dmic */
 	if (dmic_num > 0) {
-		if (ctx->ignore_pch_dmic) {
+		if (ctx->ignore_internal_dmic) {
 			dev_warn(dev, "Ignoring PCH DMIC\n");
 		} else {
 			ret = create_dmic_dailinks(card, &dai_links, &be_id);
