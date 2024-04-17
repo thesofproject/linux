1dc7311486b9751e8dc6002d33e6aeb5ba390893
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2024-04-17 13:41:43 -0500

    ASoC: sdca: parse entities
    For now only extract the ID, label and entity_type.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- include/sound/sdca_function.h
+++ include/sound/sdca_function.h
@@ -37,14 +37,63 @@ enum sdca_entity0_controls {
 	SDCA_CONTROL_ENTITY_0_FUNCTION_VERSION		= 0x08
 };
 
+/*
+ * The addressing space for SDCA relies on 7 bits for Entities, so a
+ * maximum of 128 entities per function can be represented
+ */
+#define SDCA_MAX_ENTITY_COUNT 128
+
+/*
+ * SDCA Entity Types from SDCA specification v1.a Section 6.1.2
+ * all Entity Types not described are reserved
+ */
+
+enum sdca_entity_type {
+	SDCA_ENTITY_TYPE_IT = 0x02,	/* Input Terminal */
+	SDCA_ENTITY_TYPE_OT,		/* Output Terminal */
+	SDCA_ENTITY_TYPE_MU = 0x05,	/* Mixer Unit */
+	SDCA_ENTITY_TYPE_SU,		/* Selector Unit */
+	SDCA_ENTITY_TYPE_FU,		/* Feature Unit */
+	SDCA_ENTITY_TYPE_XU = 0x0A,	/* Extension Unit */
+	SDCA_ENTITY_TYPE_CS,		/* Clock Source */
+	SDCA_ENTITY_TYPE_CX,		/* Clock selector */
+	SDCA_ENTITY_TYPE_PDE = 0x11,	/* Power-Domain Entity */
+	SDCA_ENTITY_TYPE_GE,		/* Group Entity */
+	SDCA_ENTITY_TYPE_PCE,		/* Privacy Control Entity */
+	SDCA_ENTITY_TYPE_CRU = 0x20,	/* Channel Remapping Unit */
+	SDCA_ENTITY_TYPE_UDMPU,		/* UpDownMixerUnit */
+	SDCA_ENTITY_TYPE_MFPU,		/* Multi-Function Processing Unit */
+	SDCA_ENTITY_TYPE_SMPU,		/* Smart Mic Processing Unit */
+	SDCA_ENTITY_TYPE_SAPU,		/* Smart Amp Processing Unit */
+	SDCA_ENTITY_TYPE_TG = 0x30,	/* Tone Generator */
+	SDCA_ENTITY_TYPE_HIDE		/* HID Entity */
+};
+
+/**
+ * struct sdca_entity - collection of information for one SDCA entity
+ * @id: identifier used for addressing
+ * @label: string such as "OT 12"
+ * @entity_type: identifier for that entity
+ */
+struct sdca_entity {
+	int id;
+	char *label;
+	enum sdca_entity_type entity_type;
+};
+
 /**
  * struct sdca_function - top-level information for one SDCA function
  * @adr: ACPI _ADR field reported in platform firmware
  * @topology_type: identifier for that function
+ * @num_entities: number of entities reported in this function. This is a factor
+ * of multiple options allowed in the SDCA specification
+ * @entities: dynamically allocated array of entities.
  */
 struct sdca_function {
 	u8 adr; /* limited range since only 8 functions can be supported */
 	enum sdca_function_type topology_type;
+	int num_entities;
+	struct sdca_entity *entities;
 };
 
 /**
--- sound/soc/sdca/sdca_functions.c
+++ sound/soc/sdca/sdca_functions.c
@@ -30,6 +30,95 @@ int sdca_alloc(void **context,
 }
 EXPORT_SYMBOL_NS(sdca_alloc, SND_SOC_SDCA);
 
+static int find_sdca_entities(struct device *dev,
+			      struct fwnode_handle *function_node,
+			      struct sdca_function *function)
+{
+	struct fwnode_handle *entity_node;
+	struct sdca_entity *entities;
+	char entity_property[40];
+	int num_entities;
+	u32 *entity_list;
+	u32 entity_type;
+	int ret;
+	int i;
+
+	num_entities = fwnode_property_count_u32(function_node,
+						 "mipi-sdca-entity-id-list");
+	if (num_entities >  SDCA_MAX_ENTITY_COUNT) {
+		dev_err(dev, "%s: invalid entity count %d, max allowed %d\n",
+			__func__, num_entities, SDCA_MAX_ENTITY_COUNT);
+		return -EINVAL;
+	}
+
+	entities = devm_kcalloc(dev, num_entities, sizeof(*entities), GFP_KERNEL);
+	if (!entities)
+		return -ENOMEM;
+
+	entity_list = kcalloc(num_entities, sizeof(u32), GFP_KERNEL);
+	if (!entity_list)
+		return -ENOMEM;
+
+	fwnode_property_read_u32_array(function_node,
+				       "mipi-sdca-entity-id-list",
+				       entity_list,
+				       num_entities);
+
+	for (i = 0; i < num_entities; i++)
+		entities[i].id = entity_list[i];
+
+	kfree(entity_list);
+
+	/* now read subproperties */
+	for (i = 0; i < num_entities; i++) {
+		const char *label;
+
+		/* DisCo uses upper-case for hex numbers */
+		snprintf(entity_property, sizeof(entity_property),
+			 "mipi-sdca-entity-id-0x%X-subproperties",
+			 entities[i].id);
+
+		entity_node = fwnode_get_named_child_node(function_node,
+							  entity_property);
+		if (!entity_node) {
+			dev_err(dev, "%s: %pfwP: property %s not found\n",
+				__func__, function_node, entity_property);
+			return -EINVAL;
+		}
+
+		fwnode_property_read_u32(entity_node, "mipi-sdca-entity-type",
+					 &entity_type);
+		entities[i].entity_type = entity_type;
+
+		ret = fwnode_property_read_string(entity_node, "mipi-sdca-entity-label", &label);
+		if (ret) {
+			/* Not all entities have labels, log and ignore */
+			dev_dbg(dev, "%pfwP: entity %#x property %s not found\n",
+				function, entities[i].id,
+				"mipi-sdca-entity-label");
+		} else {
+			entities[i].label = devm_kasprintf(dev, GFP_KERNEL, "%s", label);
+			if (!entities[i].label) {
+				fwnode_handle_put(entity_node);
+				return -ENOMEM;
+			}
+		}
+
+		fwnode_handle_put(entity_node);
+
+		dev_info(dev, "%s: %pfwP: found entity %#x type %#x label %s\n",
+			 __func__, function_node,
+			 entities[i].id,
+			 entities[i].entity_type,
+			 entities[i].label);
+	}
+
+	function->num_entities = num_entities;
+	function->entities = entities;
+
+	return 0;
+}
+
 static int find_sdca_function(struct acpi_device *adev, void *data)
 {
 	struct fwnode_handle *function_node = acpi_fwnode_handle(adev);
@@ -40,6 +129,7 @@ static int find_sdca_function(struct acpi_device *adev, void *data)
 	u32 function_topology = 0;
 	acpi_status status;
 	u64 addr;
+	int ret;
 
 	status = acpi_evaluate_integer(adev->handle,
 				       METHOD_NAME__ADR, NULL, &addr);
@@ -97,8 +187,16 @@ static int find_sdca_function(struct acpi_device *adev, void *data)
 
 	fwnode_handle_put(control5);
 
-	if (valid_function_found)
-		sdca->function_count++;
+	if (!valid_function_found)
+		return 0;
+
+	ret = find_sdca_entities(&adev->dev,
+				 function_node,
+				 &sdca->functions[sdca->function_count]);
+	if (ret < 0)
+		return ret;
+
+	sdca->function_count++;
 
 	return 0;
 }
