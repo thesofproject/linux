6451369cf41fef3e6f81da0bf2159cda4fc5cd9a
Vijendar Mukunda <Vijendar.Mukunda@amd.com>
2024-05-15 12:56:13 +0530

    ASoC: intel/sdw_utils: move soundwire machine driver helper functions
    Move below Intel SoundWire machine driver helper functions to
    soc_sdw_utils.c file so that it can be used by other platform machine
    driver.
    - is_unique_device()
    - get_codec_name()
    Rename the functions by adding 'asoc_sdw' tag as a prefix.
    
    Signed-off-by: Vijendar Mukunda <Vijendar.Mukunda@amd.com>

--- sound/soc/sdw_utils/soc_sdw_utils.h
+++ sound/soc/sdw_utils/soc_sdw_utils.h
@@ -64,4 +64,16 @@ int asoc_sdw_hw_params(struct snd_pcm_substream *substream,
 int asoc_sdw_hw_free(struct snd_pcm_substream *substream);
 void asoc_sdw_shutdown(struct snd_pcm_substream *substream);
 
+bool asoc_sdw_is_unique_device(const struct snd_soc_acpi_link_adr *adr_link,
+			       unsigned int sdw_version,
+			       unsigned int mfg_id,
+			       unsigned int part_id,
+			       unsigned int class_id,
+			       int index_in_link);
+
+const char *asoc_sdw_get_codec_name(struct device *dev,
+				    const struct sof_sdw_codec_info *codec_info,
+				    const struct snd_soc_acpi_link_adr *adr_link,
+				    int adr_index);
+
 #endif
