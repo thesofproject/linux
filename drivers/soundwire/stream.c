88fe8151a293adc3c0c251f6791fb1a1283afcc0
Bard Liao <yung-chuan.liao@linux.intel.com>
2024-06-13 19:44:32 +0800

    soundwire: stream: update peripheral frequency after curr_dr_freq is calculated
    Update the peripheral frequency before setting the port params as the
    curr_dr_freq might have been changed since it's initialization.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- drivers/soundwire/stream.c
+++ drivers/soundwire/stream.c
@@ -281,6 +281,9 @@ static int sdw_program_port_params(struct sdw_master_runtime *m_rt)
 	/* Program transport & port parameters for Slave(s) */
 	list_for_each_entry(s_rt, &m_rt->slave_rt_list, m_rt_node) {
 		list_for_each_entry(p_rt, &s_rt->port_list, port_node) {
+			ret = sdw_slave_set_frequency(s_rt->slave);
+			if (ret < 0)
+				return ret;
 			ret = sdw_program_slave_port_params(bus, s_rt, p_rt);
 			if (ret < 0)
 				return ret;
