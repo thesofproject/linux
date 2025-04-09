708d39b515e54c10672833c1e10fc4d2ebc86d44
Charles Keepax <ckeepax@opensource.cirrus.com>
2025-04-09 13:22:39 +0100

    soundwire: bus: Fix race on the creation of the IRQ domain
    The SoundWire IRQ domain needs to be created before any slaves are added
    to the bus, such that the domain is always available when needed. Move
    the call to sdw_irq_create() before the calls to sdw_acpi_find_slaves()
    and sdw_of_find_slaves().
    
    Fixes: 12a95123bfe1 ("soundwire: bus: Allow SoundWire peripherals to register IRQ handlers")
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Link: https://lore.kernel.org/r/20250409122239.1396489-1-ckeepax@opensource.cirrus.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/soundwire/bus.c
+++ drivers/soundwire/bus.c
@@ -124,6 +124,10 @@ int sdw_bus_master_add(struct sdw_bus *bus, struct device *parent,
 	set_bit(SDW_GROUP13_DEV_NUM, bus->assigned);
 	set_bit(SDW_MASTER_DEV_NUM, bus->assigned);
 
+	ret = sdw_irq_create(bus, fwnode);
+	if (ret)
+		return ret;
+
 	/*
 	 * SDW is an enumerable bus, but devices can be powered off. So,
 	 * they won't be able to report as present.
@@ -140,6 +144,7 @@ int sdw_bus_master_add(struct sdw_bus *bus, struct device *parent,
 
 	if (ret < 0) {
 		dev_err(bus->dev, "Finding slaves failed:%d\n", ret);
+		sdw_irq_delete(bus);
 		return ret;
 	}
 
@@ -157,10 +162,6 @@ int sdw_bus_master_add(struct sdw_bus *bus, struct device *parent,
 	bus->params.curr_dr_freq = bus->params.max_dr_freq;
 	bus->params.curr_bank = SDW_BANK0;
 	bus->params.next_bank = SDW_BANK1;
-
-	ret = sdw_irq_create(bus, fwnode);
-	if (ret)
-		return ret;
 
 	return 0;
 }
