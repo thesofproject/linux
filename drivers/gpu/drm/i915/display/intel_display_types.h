48c942fde637398c63b831683c175d3b768836a0
Jouni Högander <jouni.hogander@intel.com>
2025-09-29 16:00:02 +0300

    BACKPORT: drm/i915/alpm: Compute ALPM parameters into crtc_state->alpm_state
    Currently ALPM parameters are computed directly into
    intel_dp->alpm_parameters. This is a problem when compute config ends up to
    not using the computed state.
    
    Fix this by adding ALPM parameters into intel_crtc_state and compute into
    there. Copy needed parameters (io_wake_lines and fast_wake_lines used by
    PSR activate/exit) from crtc_state->alpm_state into
    intel_dp->alpm.alpm_parameters when they are configured into HW.
    
    v3:
      - enhance commit message
    v2:
      - store io/fast wake lines into intel_dp->dp instead of
        intel_dp->alpm_parameters and do it in intel_psr_enable_locked
      - rename crtc_state->alpm_parameters -> crtc_state->alpm_state
      - clarify commit message
    
    Signed-off-by: Jouni Högander <jouni.hogander@intel.com>
    Reviewed-by: Animesh Manna <animesh.manna@intel.com>
    Link: https://lore.kernel.org/r/20250929130003.28365-1-jouni.hogander@intel.com
    (cherry picked from commit 2bc98c6f97afaa55d7d1d3b66ea850166180d4fa)
    
    Bug: 455442294
    Bug: 481847741
    
    Test: VRR
    Change-Id: Ie1b9274859b4349e5b8616b51d3c9e131f388cb3
    Signed-off-by: Ap, Kamal <kamal.ap@intel.corp-partner.google.com>

--- drivers/gpu/drm/i915/display/intel_display_types.h
+++ drivers/gpu/drm/i915/display/intel_display_types.h
@@ -1356,6 +1356,17 @@ struct intel_crtc_state {
 	/* W2 window or 'set context latency' lines */
 	u16 set_context_latency;
 
+	struct {
+		u8 io_wake_lines;
+		u8 fast_wake_lines;
+
+		/* LNL and beyond */
+		u8 check_entry_lines;
+		u8 aux_less_wake_lines;
+		u8 silence_period_sym_clocks;
+		u8 lfps_half_cycle_num_of_syms;
+	} alpm_state;
+
 	/* to track changes in plane color blocks */
 	bool plane_color_changed;
 };
@@ -1703,6 +1714,9 @@ struct intel_psr {
 	struct delayed_work dc3co_work;
 	u8 entry_setup_frames;
 
+	u8 io_wake_lines;
+	u8 fast_wake_lines;
+
 	bool link_ok;
 	bool pkg_c_latency_used;
 
@@ -1862,16 +1876,9 @@ struct intel_dp {
 	bool colorimetry_support;
 
 	struct {
-		u8 io_wake_lines;
-		u8 fast_wake_lines;
 		enum transcoder transcoder;
 		struct mutex lock;
 
-		/* LNL and beyond */
-		u8 check_entry_lines;
-		u8 aux_less_wake_lines;
-		u8 silence_period_sym_clocks;
-		u8 lfps_half_cycle_num_of_syms;
 		bool lobf_disable_debug;
 		bool sink_alpm_error;
 	} alpm_parameters;
