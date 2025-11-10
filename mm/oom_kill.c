6d29e38680f73f931a33fb5bb784e769a94d5c04
Jaewon Kim <jaewon31.kim@samsung.com>
2025-11-10 22:51:23 +0900

    ANDROID: mm: add vendor hook to oom victim and disable
    The oom killer disable timeout may happen if the oom victim tasks did
    not die. To get some clues, add vendor hook to leave oom victim
    information.
    
    Bug: 459380738
    Change-Id: I2ac17a839bcb678bb241f6eb0d1eb3ae3dbbbb6d
    Signed-off-by: Jaewon Kim <jaewon31.kim@samsung.com>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -813,6 +813,7 @@ void exit_oom_victim(void)
 {
 	clear_thread_flag(TIF_MEMDIE);
 
+	trace_android_vh_exit_oom_victim(current);
 	if (!atomic_dec_return(&oom_victims))
 		wake_up_all(&oom_victims_wait);
 }
@@ -857,6 +858,7 @@ bool oom_killer_disable(signed long timeout)
 	ret = wait_event_interruptible_timeout(oom_victims_wait,
 			!atomic_read(&oom_victims), timeout);
 	if (ret <= 0) {
+		trace_android_vh_oom_killer_disable(atomic_read(&oom_victims));
 		oom_killer_enable();
 		return false;
 	}
