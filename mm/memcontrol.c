BayLibre/android_kernel_common

ANDROID: mm: export lruvec_page_state_local
Export lruvec_page_state_local to support getting the sizes of specified types, such as anonymous pages and file pages, in ko.

Bug: 394491702
Change-Id: I7d78799bf9ec0465a912e817f9475179b3715de9
Signed-off-by: yipeng xiang <yipengxiang@honor.corp-partner.google.com>

mm/memcontrol.c
@@ -412,6 +412,7 @@ unsigned long lruvec_page_state_local(struct lruvec *lruvec,
 #endif
 	return x;
 }
+EXPORT_SYMBOL_GPL(lruvec_page_state_local);
 
 /* Subset of vm_event_item to report for memcg event stats */
 static const unsigned int memcg_vm_event_stat[] = {

thesofproject/linux

(cherry picked from commit afe2ea85cf1ebdcf846f159840af242c4c353d96)

mm/memcontrol.c
@@ -409,6 +409,7 @@ unsigned long lruvec_page_state(struct lruvec *lruvec, enum node_stat_item idx)
 #endif
 	return x;
 }
+EXPORT_SYMBOL_GPL(lruvec_page_state);
 
 unsigned long lruvec_page_state_local(struct lruvec *lruvec,
 				      enum node_stat_item idx)