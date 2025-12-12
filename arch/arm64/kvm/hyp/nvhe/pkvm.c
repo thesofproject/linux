BayLibre/android_kernel_common

ANDROID: KVM: arm64: iommu: Add hyp_pool for guest pvIOMMU
Hyp pool would be used to cache guest allocations for the IOMMU, so
when they are freed they can be used again.

Also add a memcache for teardown of the pool.

Bug: 357781595
Bug: 348382247
Bug: 236685427
Change-Id: I43b0e097017efef4147ee5e94c7a77b4a251acb8
Signed-off-by: Mostafa Saleh <smostafa@google.com>

arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -368,7 +368,7 @@ int __pkvm_reclaim_dying_guest_page(pkvm_handle_t handle, u64 pfn, u64 gfn, u8 o
 	if (ret)
 		goto unlock;
 
-	drain_hyp_pool(hyp_vm, &hyp_vm->host_kvm->arch.pkvm.stage2_teardown_mc);
+	drain_hyp_pool(&hyp_vm->pool, &hyp_vm->host_kvm->arch.pkvm.stage2_teardown_mc);
 unlock:
 	hyp_read_unlock(&vm_table_lock);
 
@@ -965,6 +965,13 @@ int __pkvm_finalize_teardown_vm(pkvm_handle_t handle)
 	pkvm_pviommu_teardown(hyp_vm);
 
 	/*
+	 * At this point all page tables are destroyed and should be pushed to the pool
+	 * the only place that might still have memory is the mc, which would be drained
+	 * from host as it hasn't been donated yet.
+	 */
+	drain_hyp_pool(&hyp_vm->iommu_pool, &host_kvm->arch.pkvm.teardown_iommu_mc);
+
+	/*
 	 * At this point, the VM has been detached from the VM table and
 	 * has a refcount of 0 so we're free to tear it down without
 	 * worrying about anybody else.
@@ -972,7 +979,7 @@ int __pkvm_finalize_teardown_vm(pkvm_handle_t handle)
 
 	mc = &host_kvm->arch.pkvm.stage2_teardown_mc;
 	destroy_hyp_vm_pgt(hyp_vm);
-	drain_hyp_pool(hyp_vm, mc);
+	drain_hyp_pool(&hyp_vm->pool, mc);
 	unpin_host_vcpus(hyp_vm->vcpus, hyp_vm->kvm.created_vcpus);
 
 	/* Push the metadata pages to the teardown memcache */

arch/arm64/kvm/pkvm.c
@@ -403,6 +403,8 @@ out_free:
 		     &host_kvm->stat.protected_hyp_mem);
 	free_hyp_memcache(&host_kvm->arch.pkvm.stage2_teardown_mc);
 
+	kvm_iommu_guest_free_mc(&host_kvm->arch.pkvm.teardown_iommu_mc);
+
 	kvm_for_each_vcpu(idx, host_vcpu, host_kvm) {
 		struct kvm_hyp_req *hyp_reqs = host_vcpu->arch.hyp_reqs;
 

thesofproject/linux

Bug: 458241298

arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -1060,7 +1060,7 @@ int __pkvm_reclaim_dying_guest_page(pkvm_handle_t handle, u64 gfn, u64 nr_pages)
 	if (ret)
 		goto unlock;
 
-	drain_hyp_pool(hyp_vm, &hyp_vm->host_kvm->arch.pkvm.stage2_teardown_mc);
+	drain_hyp_pool(&hyp_vm->pool, &hyp_vm->host_kvm->arch.pkvm.stage2_teardown_mc);
 unlock:
 	hyp_read_unlock(&vm_table_lock);
 
@@ -1128,6 +1128,13 @@ int __pkvm_finalize_teardown_vm(pkvm_handle_t handle)
 	pkvm_pviommu_teardown(hyp_vm);
 
 	/*
+	 * At this point all page tables are destroyed and should be pushed to the pool
+	 * the only place that might still have memory is the mc, which would be drained
+	 * from host as it hasn't been donated yet.
+	 */
+	drain_hyp_pool(&hyp_vm->iommu_pool, &host_kvm->arch.pkvm.teardown_iommu_mc);
+
+	/*
 	 * At this point, the VM has been detached from the VM table and
 	 * has a refcount of 0 so we're free to tear it down without
 	 * worrying about anybody else.
@@ -1135,7 +1142,7 @@ int __pkvm_finalize_teardown_vm(pkvm_handle_t handle)
 
 	stage2_mc = &host_kvm->arch.pkvm.stage2_teardown_mc;
 	destroy_hyp_vm_pgt(hyp_vm);
-	drain_hyp_pool(hyp_vm, stage2_mc);
+	drain_hyp_pool(&hyp_vm->pool, stage2_mc);
 	unpin_host_vcpus(hyp_vm->vcpus, hyp_vm->kvm.created_vcpus);
 
 	/* Push the metadata pages to the teardown memcache */

arch/arm64/kvm/pkvm.c
@@ -292,6 +292,7 @@ static void __pkvm_finalize_destroy_hyp_vm(struct kvm *kvm)
 	atomic64_sub(kvm->arch.pkvm.stage2_teardown_mc.nr_pages << PAGE_SHIFT,
 		     &kvm->stat.protected_hyp_mem);
 	free_hyp_memcache(&kvm->arch.pkvm.stage2_teardown_mc);
+	kvm_iommu_guest_free_mc(&kvm->arch.pkvm.teardown_iommu_mc);
 
 	kvm_for_each_vcpu(idx, vcpu, kvm) {
 		struct kvm_hyp_req *hyp_reqs = vcpu->arch.hyp_reqs;