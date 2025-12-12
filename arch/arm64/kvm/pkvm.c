CodeLinaro-mirror/kernel-platform-2-0-os_kernel_common

ANDROID: KVM: arm64: iommu: Add memcache for guest pvIOMMU
Guest pvIOMMU would need memory for page tables, this is done through
the host donating pages to the guest, and will be owned by the
hypervisor as the guest has no direct access (or knowledge) of the
IOMMU page tables.

This is done through a new memcache per vcpu dedicated for each
vcpu.

And the requests for memory will be communiacted via kvm_hyp_req and
fullfilled by the IOMMU driver or with a default function.

Change-Id: I8a32072dc21879165e7921dad83f312447360539
Signed-off-by: Mostafa Saleh <smostafa@google.com>

arch/arm64/kvm/pkvm.c
@@ -403,6 +403,8 @@ out_free:
 		kvm_unshare_hyp(hyp_reqs, hyp_reqs + 1);
 		host_vcpu->arch.hyp_reqs = NULL;
 		free_page((unsigned long)hyp_reqs);
+
+		kvm_iommu_guest_free_mc(&host_vcpu->arch.iommu_mc);
 	}
 }
 

thesofproject/linux

Bug: 357781595
Bug: 348382247
Bug: 236685427
Bug: 458241298

arch/arm64/kvm/pkvm.c
@@ -302,6 +302,8 @@ static void __pkvm_finalize_destroy_hyp_vm(struct kvm *kvm)
 		kvm_unshare_hyp(hyp_reqs, hyp_reqs + 1);
 		vcpu->arch.hyp_reqs = NULL;
 		free_page((unsigned long)hyp_reqs);
+
+		kvm_iommu_guest_free_mc(&vcpu->arch.iommu_mc);
 	}
 }
 