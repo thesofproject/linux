BayLibre/android_kernel_common

ANDROID: 16K: proc: Scale page-based task memory counters
When emulating a 16KB page size, procfs files that report memory
usage in units of pages must be adjusted to reflect the userspace
page size. Otherwise, tools that read these files will miscalculate
memory usage by a factor of four.

This affects Perfetto's traced_probes watchdog, which reads the RSS
counter from /proc/[pid]/stat and was incorrectly killing the
process for exceeding its memory limit.

Introduce a helper function, __page_size_count(), to scale page
counts from the native 4KB PAGE_SIZE to the emulated 16KB
__PAGE_SIZE.

Apply this scaling to the counters in the following procfs files,
which report values in pages:
- /proc/[pid]/stat: (rss)
- /proc/[pid]/statm: (size, resident, shared, text, data)

This ensures that memory usage is reported consistently with the
emulated environment, resolving the watchdog crashes.

Bug: 469233272
Bug: 442871407
Bug: 442872029
Bug: 332718696
Change-Id: I8557c36a573f156264de13b706f1a8c942562cea
Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

fs/proc/array.c
@@ -73,6 +73,7 @@
 #include <linux/mm.h>
 #include <linux/hugetlb.h>
 #include <linux/pagemap.h>
+#include <linux/page_size_compat.h>
 #include <linux/swap.h>
 #include <linux/smp.h>
 #include <linux/signal.h>
@@ -611,7 +612,7 @@ static int do_task_stat(struct seq_file *m, struct pid_namespace *ns,
 	seq_put_decimal_ull(m, " ", 0);
 	seq_put_decimal_ull(m, " ", start_time);
 	seq_put_decimal_ull(m, " ", vsize);
-	seq_put_decimal_ull(m, " ", mm ? get_mm_rss(mm) : 0);
+	seq_put_decimal_ull(m, " ", mm ? __page_size_count(get_mm_rss(mm)) : 0);
 	seq_put_decimal_ull(m, " ", rsslim);
 	seq_put_decimal_ull(m, " ", mm ? (permitted ? mm->start_code : 1) : 0);
 	seq_put_decimal_ull(m, " ", mm ? (permitted ? mm->end_code : 1) : 0);

thesofproject/linux

Bug: 481846501
Signed-off-by: Adrian Barnaś <abarnas@google.com>

fs/proc/array.c
@@ -73,6 +73,7 @@
 #include <linux/mm.h>
 #include <linux/hugetlb.h>
 #include <linux/pagemap.h>
+#include <linux/page_size_compat_defs.h>
 #include <linux/swap.h>
 #include <linux/smp.h>
 #include <linux/signal.h>
@@ -609,7 +610,7 @@ static int do_task_stat(struct seq_file *m, struct pid_namespace *ns,
 	seq_put_decimal_ull(m, " ", 0);
 	seq_put_decimal_ull(m, " ", start_time);
 	seq_put_decimal_ull(m, " ", vsize);
-	seq_put_decimal_ull(m, " ", mm ? get_mm_rss(mm) : 0);
+	seq_put_decimal_ull(m, " ", mm ? __page_size_count(get_mm_rss(mm)) : 0);
 	seq_put_decimal_ull(m, " ", rsslim);
 	seq_put_decimal_ull(m, " ", mm ? (permitted ? mm->start_code : 1) : 0);
 	seq_put_decimal_ull(m, " ", mm ? (permitted ? mm->end_code : 1) : 0);