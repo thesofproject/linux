BayLibre/android_kernel_common

ANDROID: pKVM: VMX: Intercept MSR reading for protected apic
With APICv, all the readable apic registers doesn't need to be
intercepted. However the protected apic still have some registers
emulated by the host. The x2apic MSRs corresponding to such registers
should be intercepted to make sure the guest always gets up-to-date
values from the host via the emulation, instead of reading stale values
from the apic page directly.

BUG: 489390184

Change-Id: I3aeb6d3b4e0095b1056f319f25de840559c28496
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/vmx.c
@@ -4412,10 +4412,26 @@ static void vmx_update_msr_bitmap_x2apic(struct kvm_vcpu *vcpu)
 	 * through reads for all valid registers by default in x2APIC+APICv
 	 * mode, only the current timer count needs on-demand emulation by KVM.
 	 */
-	if (mode & MSR_BITMAP_MODE_X2APIC_APICV)
+	if (mode & MSR_BITMAP_MODE_X2APIC_APICV) {
+#ifndef __PKVM_HYP__
 		msr_bitmap[read_idx] = ~kvm_lapic_readable_reg_mask(vcpu->arch.apic);
-	else
+#else
+		if (vcpu->arch.apic->guest_apic_protected)
+			/*
+			 * The protected apic still have some registers emulated by the
+			 * host. Such registers should be intercepted to make sure the
+			 * guest always gets up-to-date values from the host via
+			 * the emulation, instead of reading stale values from the apic
+			 * page directly.
+			 */
+			msr_bitmap[read_idx] =
+				~pkvm_protected_lapic_readable_reg_mask(vcpu->arch.apic);
+		else
+			msr_bitmap[read_idx] = ~kvm_lapic_readable_reg_mask(vcpu->arch.apic);
+#endif
+	} else {
 		msr_bitmap[read_idx] = ~0ull;
+	}
 	msr_bitmap[write_idx] = ~0ull;
 
 	/*

thesofproject/linux

Bug: 489390184

arch/x86/kvm/vmx/vmx.c
@@ -4412,10 +4412,23 @@ static void vmx_update_msr_bitmap_x2apic(struct kvm_vcpu *vcpu)
 	 * through reads for all valid registers by default in x2APIC+APICv
 	 * mode, only the current timer count needs on-demand emulation by KVM.
 	 */
-	if (mode & MSR_BITMAP_MODE_X2APIC_APICV)
+	if (mode & MSR_BITMAP_MODE_X2APIC_APICV) {
 		msr_bitmap[read_idx] = ~kvm_lapic_readable_reg_mask(vcpu->arch.apic);
-	else
+#ifdef __PKVM_HYP__
+		if (vcpu->arch.apic->guest_apic_protected)
+			/*
+			 * The protected apic still has some registers emulated by the
+			 * host. Such registers should be intercepted to make sure the
+			 * guest always gets up-to-date values from the host via
+			 * the emulation, instead of reading stale values from the apic
+			 * page directly.
+			 */
+			msr_bitmap[read_idx] =
+				~pkvm_protected_lapic_readable_reg_mask(vcpu->arch.apic);
+#endif
+	} else {
 		msr_bitmap[read_idx] = ~0ull;
+	}
 	msr_bitmap[write_idx] = ~0ull;
 
 	/*