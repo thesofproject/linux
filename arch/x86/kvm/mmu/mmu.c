d122a6adbe0bbcedfae4e26f23632a94fc4e6353
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-04-14 09:50:25 +0800

    ANDROID: KVM: x86/mmu: pKVM: Fix page reference leak in pkvm_page_fault()
    When kvm_mmu_faultin_pfn() returns RET_PF_CONTINUE, it may have stored a
    page reference in fault->refcounted_page. If kvm_topup_pkvm_memcache()
    or kzalloc() subsequently fails, the early returns bypass
    kvm_mmu_finish_page_fault(), leaking the page reference.
    
    So add releasing the faulted-in page on these error paths.
    
    Fixes: d8ae80f9eac9 ("ANDROID: KVM: x86/mmu: pKVM: Implement page fault handler")
    Change-Id: Ie87e63ce6510a7dc1305fcea29408fa589b1762b
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -5043,12 +5043,14 @@ static int pkvm_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 	r = kvm_topup_pkvm_memcache(&vcpu->arch.pkvm.guest_mmu_memcache,
 				    pkvm_mmu_cache_min_pages());
 	if (r)
-		return r;
+		goto out_release_pfn;
 
 	/* Allocate non-atomically before taking mmu_lock. */
 	mapping = kzalloc(sizeof(struct pkvm_mapping), GFP_KERNEL_ACCOUNT);
-	if (!mapping)
-		return -ENOMEM;
+	if (!mapping) {
+		r = -ENOMEM;
+		goto out_release_pfn;
+	}
 
 	r = RET_PF_RETRY;
 	write_lock(&vcpu->kvm->mmu_lock);
@@ -5110,6 +5112,10 @@ out_unlock:
 
 	if (r != RET_PF_FIXED)
 		kfree(mapping);
+	return r;
+out_release_pfn:
+	kvm_release_faultin_page(vcpu->kvm, fault->refcounted_page,
+				 true, fault->map_writable);
 	return r;
 }
 #endif /* CONFIG_PKVM_X86 */
