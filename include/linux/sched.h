cd4c71bbe4e8220332b4d9eff7d65a113ff1aa41
kuyo chang <kuyo.chang@mediatek.corp-partner.google.com>
2026-05-06 14:34:46 +0800

    ANDROID: Correct the memory layout for dmabuf
    With the current design, vendor data is appended to the end
    of struct task_struct according to the DTS configuration,
    and accessed via android_task_vendor_data(p).
    
    However, as has been the design in Linux for many years,
    struct thread_struct thread; should be the last field in struct task_struct.
    Move struct task_dma_buf_info *dmabuf_info;
    to be placed before:
    struct thread_struct thread;
    so that the layout remains consistent with the long-standing Linux design.
    
    Fixes: 7586bc159720 ("ANDROID: Track per-process dmabuf RSS")
    Bug: 510105118
    Change-Id: I4f61ac7f24d5ce62b12bda6315b30df9f98047a4
    Signed-off-by: kuyo chang <kuyo.chang@mediatek.corp-partner.google.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1711,10 +1711,10 @@ struct task_struct {
 	struct unwind_task_info		unwind_info;
 #endif
 
+	struct task_dma_buf_info *dmabuf_info;
+
 	/* CPU-specific state of this task: */
 	struct thread_struct		thread;
-
-	struct task_dma_buf_info *dmabuf_info;
 
 	/*
 	 * New fields for task_struct should be added above here, so that
