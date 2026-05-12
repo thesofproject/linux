maltesermailo/android_kernel_pantah

ANDROID: rust_binder: vh for starting/stopping being a binder thread
Some vendor hooks keep track of whether a given thread is a binder
thread. To allow for this, introduce a vendorhook that's called whenever
a thread starts or stops being a binder thread.

Bug: 486143390
Change-Id: I5dc71d6d74c6883456e87d5c24fe1fbf2b79eaf9
Signed-off-by: Alice Ryhl <aliceryhl@google.com>

drivers/android/binder/rust_binder.h
@@ -169,4 +169,14 @@ static inline binder_uintptr_t rust_binder_node_ptr(rust_binder_node t)
 	return * (binder_uintptr_t *) (t + RUST_BINDER_LAYOUT.n.ptr);
 }
 
+/*
+ * Constants for bitflag passed to `android_vh_rust_binder_looper_entry`. A
+ * registered looper is one created after the driver requested its creation
+ * with BR_SPAWN_LOOPER, and an entered looper is one that userspace decided to
+ * create on its own volition.
+ */
+#define LOOPER_REGISTERED 0x01
+#define LOOPER_ENTERED 0x02
+#define LOOPER_EXITED 0x04
+
 #endif

thesofproject/linux

drivers/android/binder/rust_binder.h
@@ -178,4 +178,14 @@ static inline binder_uintptr_t rust_binder_node_ptr(rust_binder_node t)
 	return *(binder_uintptr_t *) (t + RUST_BINDER_LAYOUT.n.ptr);
 }
 
+/*
+ * Constants for bitflag passed to `android_vh_rust_binder_looper_entry`. A
+ * registered looper is one created after the driver requested its creation
+ * with BR_SPAWN_LOOPER, and an entered looper is one that userspace decided to
+ * create on its own volition.
+ */
+#define RB_LOOPER_REGISTERED 0x01
+#define RB_LOOPER_ENTERED 0x02
+#define RB_LOOPER_EXITED 0x04
+
 #endif