maltesermailo/android_kernel_pantah

ANDROID: mm: Export several symbols
Exports several symbols which are used in the vendor ko.

mte_sync_tags - It is used by the macro set_pte_at.

mthp_stats - It is used by mod_mthp_stat().

swapper_spaces - It is used by macro swap_address_space.

swap_migration_ad_supported - It is used by
make_migration_entry_young().

Bug: 431672372
Change-Id: I5f61af22ed9c11cd5c5a79eaa49a62c5153693b9
Signed-off-by: Pengfei Li <pengfei.kernel@vivo.corp-partner.google.com>

mm/swap_state.c
@@ -39,6 +39,7 @@ static const struct address_space_operations swap_aops = {
 };
 
 struct address_space *swapper_spaces[MAX_SWAPFILES] __read_mostly;
+EXPORT_SYMBOL_GPL(swapper_spaces);
 static unsigned int nr_swapper_spaces[MAX_SWAPFILES] __read_mostly;
 static bool enable_vma_readahead __read_mostly = true;
 

thesofproject/linux

swap_space - It is used by swap_address_space().
__mmu_notifier_invalidate_range_start
__mmu_notifier_arch_invalidate_secondary_tlbs
__mmu_notifier_invalidate_range_end - These three functions are used in
vendors ko to invalidate and refresh the tlb.
Bug: 505636392
[ Resolved minor conflicts about swap_space - Pengfei Li ]
(cherry picked from commit 8a9183c5070d253367df1933eb6e6b30da7d91ba)

mm/swap_state.c
@@ -40,6 +40,7 @@ static const struct address_space_operations swap_aops = {
 struct address_space swap_space __read_mostly = {
 	.a_ops = &swap_aops,
 };
+EXPORT_SYMBOL_GPL(swap_space);
 
 static bool enable_vma_readahead __read_mostly = true;
 