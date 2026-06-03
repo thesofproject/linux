BayLibre/android_kernel_common

ANDROID: mm: use vma_data_pages for readahead limit
Use the android specific vma_data_pages function to limit readahead to
the end of the VMA's data region so that we don't prefetch zeroes into
the cache.

Bug: 499041092
Change-Id: I04e0828a4d4023e880b1904c5a48d8debf0e1e49
Signed-off-by: Frederick Mayle <fmayle@google.com>

mm/filemap.c
@@ -44,6 +44,7 @@
 #include <linux/ramfs.h>
 #include <linux/page_idle.h>
 #include <linux/page_size_compat.h>
+#include <linux/pgsize_migration.h>
 #include <linux/migrate.h>
 #include <linux/pipe_fs_i.h>
 #include <linux/splice.h>
@@ -3331,7 +3332,7 @@ static struct file *do_sync_mmap_readahead(struct vm_fault *vmf)
 	}
 #endif
 
-	ractl.max_index = vmf->vma->vm_pgoff + vma_pages(vmf->vma) - 1;
+	ractl.max_index = vmf->vma->vm_pgoff + vma_data_pages(vmf->vma) - 1;
 
 	/*
 	 * If we don't want any read-ahead, don't bother. VM_EXEC case below is
@@ -3446,7 +3447,7 @@ static struct file *do_async_mmap_readahead(struct vm_fault *vmf,
 	}
 
 	if (folio_test_readahead(folio)) {
-		ractl.max_index = vmf->vma->vm_pgoff + vma_pages(vmf->vma) - 1;
+		ractl.max_index = vmf->vma->vm_pgoff + vma_data_pages(vmf->vma) - 1;
 		fpin = maybe_unlock_mmap_for_io(vmf, fpin);
 		trace_android_vh_page_cache_readahead_start(file, vmf->pgoff,
 				ra->ra_pages, false);

thesofproject/linux

mm/filemap.c
@@ -44,6 +44,7 @@
 #include <linux/ramfs.h>
 #include <linux/page_idle.h>
 #include <linux/page_size_compat.h>
+#include <linux/pgsize_migration.h>
 #include <linux/migrate.h>
 #include <linux/pipe_fs_i.h>
 #include <linux/splice.h>
@@ -3325,7 +3326,7 @@ static struct file *do_sync_mmap_readahead(struct vm_fault *vmf)
 	if (skip)
 		return fpin;
 
-	ractl._max_index = vmf->vma->vm_pgoff + vma_pages(vmf->vma) - 1;
+	ractl._max_index = vmf->vma->vm_pgoff + vma_data_pages(vmf->vma) - 1;
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	/* Use the readahead code, even if readahead is disabled */
@@ -3390,7 +3391,7 @@ static struct file *do_sync_mmap_readahead(struct vm_fault *vmf)
 		 */
 		struct vm_area_struct *vma = vmf->vma;
 		unsigned long start = vma->vm_pgoff;
-		unsigned long end = start + vma_pages(vma);
+		unsigned long end = start + vma_data_pages(vma);
 		unsigned long ra_end;
 
 		ra->order = exec_folio_order();
@@ -3459,7 +3460,7 @@ static struct file *do_async_mmap_readahead(struct vm_fault *vmf,
 	}
 
 	if (folio_test_readahead(folio)) {
-		ractl._max_index = vmf->vma->vm_pgoff + vma_pages(vmf->vma) - 1;
+		ractl._max_index = vmf->vma->vm_pgoff + vma_data_pages(vmf->vma) - 1;
 		fpin = maybe_unlock_mmap_for_io(vmf, fpin);
 		trace_android_vh_page_cache_readahead_start(file, vmf->pgoff,
 				ra->ra_pages, false);