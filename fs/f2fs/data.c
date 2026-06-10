ebe4c102f35e7b8d60a4e8492296a46a10638f53
Bart Van Assche <bvanassche@acm.org>
2026-06-10 12:34:17 -0700

    UPSTREAM: f2fs: Prepare for supporting delayed bio completion
    Use bio frontpadding to allocate memory for a work_struct when
    allocating a bio.
    
    Bug: 507580336
    Reviewed-by: Chao Yu <chao@kernel.org>
    Change-Id: I28e84f58c52679e0e868ac523f09cc89ec9146a3
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit 41b7928813b5db9f61bf55e0ce395f69eb0473eb)

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -44,6 +44,11 @@ struct f2fs_folio_state {
 	unsigned int		read_pages_pending;
 };
 
+struct f2fs_bio {
+	struct work_struct work;
+	struct bio bio;
+};
+
 #define	F2FS_BIO_POOL_SIZE	NR_CURSEG_TYPE
 
 EXPORT_TRACEPOINT_SYMBOL_GPL(f2fs_write_begin);
@@ -51,7 +56,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(f2fs_write_begin);
 int __init f2fs_init_bioset(void)
 {
 	return bioset_init(&f2fs_bioset, F2FS_BIO_POOL_SIZE,
-					0, BIOSET_NEED_BVECS);
+			   offsetof(struct f2fs_bio, bio), BIOSET_NEED_BVECS);
 }
 
 void f2fs_destroy_bioset(void)
