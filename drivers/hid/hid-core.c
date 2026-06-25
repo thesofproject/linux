28ef5ece6941de3c35c2ba64ea6a9a2a8578d350
Lee Jones <joneslee@google.com>
2026-06-25 11:59:55 +0100

    ANDROID: Fix ABI issues related to changed hid_report_raw_event()
    Upstream recently started passing the original buffer size through
    hid_report_raw_event() in order to fix a few recently reported
    vulnerabilities.  These issues also affect Android so we need this
    change.  However, since it changes the function signature which breaks
    ABI, we need to provide a wrapper.
    
    Bug: 483018448
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: I2c50f89815a713391a6ae9510601a3a0ebfc4d5e

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -2029,8 +2029,8 @@ out:
 }
 EXPORT_SYMBOL_GPL(__hid_request);
 
-int hid_report_raw_event(struct hid_device *hid, enum hid_report_type type, u8 *data,
-			 size_t bufsize, u32 size, int interrupt)
+int __hid_report_raw_event(struct hid_device *hid, enum hid_report_type type, u8 *data,
+			   size_t bufsize, u32 size, int interrupt)
 {
 	struct hid_report_enum *report_enum = hid->report_enum + type;
 	struct hid_report *report;
@@ -2099,6 +2099,13 @@ int hid_report_raw_event(struct hid_device *hid, enum hid_report_type type, u8 *
 
 	return ret;
 }
+EXPORT_SYMBOL_GPL(__hid_report_raw_event);
+
+int hid_report_raw_event(struct hid_device *hid, enum hid_report_type type, u8 *data, u32 size,
+			 int interrupt)
+{
+	return __hid_report_raw_event(hid, type, data, size, size, interrupt);
+}
 EXPORT_SYMBOL_GPL(hid_report_raw_event);
 
 
@@ -2159,7 +2166,7 @@ static int __hid_input_report(struct hid_device *hid, enum hid_report_type type,
 			goto unlock;
 	}
 
-	ret = hid_report_raw_event(hid, type, data, bufsize, size, interrupt);
+	ret = __hid_report_raw_event(hid, type, data, bufsize, size, interrupt);
 
 unlock:
 	if (!lock_already_taken)
