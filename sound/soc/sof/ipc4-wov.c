a9f36783c76a5e483c71045ca5d5d48034adaf91
Naveen Manohar <naveen.m@intel.com>
2026-08-25 22:35:33 +0530

    ASoC: SOF: ipc4: notify WoV kcontrols on PHRASE_DETECTED
    When DSP reports keyword detection, notify topology-defined
    WoV kcontrols so that user space can poll for value changes.
    
    Checked for 2 kcontrols in loaded topology:
    - 'wov_trigger_id': RO enum reporting winner slot(0/1/2)
    - 'wov_event': control that user space polls via POLLPRI
    
    Signed-off-by: Naveen Manohar <naveen.m@intel.com>

--- sound/soc/sof/ipc4-wov.c
+++ sound/soc/sof/ipc4-wov.c
@@ -4,7 +4,10 @@
 
 #include <linux/bits.h>
 #include <sound/pcm.h>
+#include <sound/control.h>
+#include <sound/soc.h>
 #include "sof-audio.h"
+#include "sof-priv.h"
 #include "ipc4-wov.h"
 
 /* IPC4 PHRASE_DETECTED primary/extension field layout */
@@ -14,6 +17,56 @@
 
 /* PCM ID for WoV keyword detection capture stream */
 #define SOF_WOV_PCM_ID	11
+
+/* kcontrol names as defined in the dmic-wov feature topology */
+#define SOF_WOV_KEYWORD_ID_CTL	"wov_trigger_id"
+#define SOF_WOV_EVENT_CTL	"wov_event"
+
+/* sof_ipc4_wov_notify_kcontrol_by_name - Locate mixer control by name and notify */
+static void sof_ipc4_wov_notify_kcontrol_by_name(struct snd_card *card,
+						 const char *name)
+{
+	struct snd_ctl_elem_id id;
+	struct snd_kcontrol *kctl;
+
+	if (!card || !name)
+		return;
+
+	memset(&id, 0, sizeof(id));
+	id.iface = SNDRV_CTL_ELEM_IFACE_MIXER;
+	strscpy(id.name, name, sizeof(id.name));
+
+	kctl = snd_ctl_find_id(card, &id);
+	if (kctl)
+		snd_ctl_notify_one(card, SNDRV_CTL_EVENT_MASK_VALUE, kctl, 0);
+}
+
+/* sof_ipc4_wov_notify_kcontrols - Iterate SOF controls and notify userspace */
+static void sof_ipc4_wov_notify_kcontrols(struct snd_sof_dev *sdev)
+{
+	static const char * const wov_ctl_names[] = {
+		SOF_WOV_KEYWORD_ID_CTL,
+		SOF_WOV_EVENT_CTL,
+	};
+	struct snd_sof_control *scontrol;
+	int i;
+
+	list_for_each_entry(scontrol, &sdev->kcontrol_list, list) {
+		for (i = 0; i < ARRAY_SIZE(wov_ctl_names); i++) {
+			if (strcmp(scontrol->name, wov_ctl_names[i]))
+				continue;
+
+			/* Force firmware re-read on next .get */
+			scontrol->comp_data_dirty = true;
+
+			if (scontrol->scomp && scontrol->scomp->card) {
+				sof_ipc4_wov_notify_kcontrol_by_name(
+					scontrol->scomp->card->snd_card,
+					scontrol->name);
+			}
+		}
+	}
+}
 
 void sof_ipc4_wov_phrase_detected(struct snd_sof_dev *sdev,
 				  struct sof_ipc4_msg *ipc4_msg)
@@ -29,6 +82,10 @@ void sof_ipc4_wov_phrase_detected(struct snd_sof_dev *sdev,
 
 	pm_wakeup_event(sdev->dev, 2000);
 
+	/* 1. Notify WoV kcontrols */
+	sof_ipc4_wov_notify_kcontrols(sdev);
+
+	/* 2. Unblock capture PCM stream */
 	list_for_each_entry(spcm, &sdev->pcm_list, list) {
 		if (le32_to_cpu(spcm->pcm.pcm_id) != SOF_WOV_PCM_ID)
 			continue;
